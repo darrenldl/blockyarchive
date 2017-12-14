56c59d09d38dfdbb94713bf76a6bdb595578a280
Darren Ldl <darrenldldev@gmail.com>
2017-12-14 14:05:00 +1100

    Added basic test code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,36 @@
 extern crate mpmc_rb;
 
-fn main() {
+use mpmc_rb::channel;
+use std::thread;
 
+fn main() {
+    let (tx, rx) = channel::<String>(1);
+
+    let mut v = vec![];
+
+    for i in 1..2 {
+        let tx = tx.clone();
+        let handle = thread::spawn(move || {
+            loop {
+                let msg = String::from("abcd");
+                println!("Sender {}, sending {}", i, msg);
+                tx.send(msg).unwrap();
+            }
+        });
+        v.push(handle);
+    }
+
+    for i in 1..5 {
+        let rx = rx.clone();
+        let handle = thread::spawn(move || {
+            loop {
+                println!("Receiver {}, received {:?}", i, rx.recv());
+            }
+        });
+        v.push(handle);
+    }
+
+    for i in v.into_iter() {
+        i.join().unwrap();
+    }
 }
