f565866ff04802c1cde7d46c875ed8fd53909429
Darren Ldl <darrenldldev@gmail.com>
2017-12-15 04:38:48 +1100

    Added get set functions

--- reed_solomon/src/matrix.rs
+++ reed_solomon/src/matrix.rs
@@ -5,17 +5,21 @@ pub struct Matrix {
 }
 
 impl Matrix {
-    fn new(rows : usize, cols : usize) -> Matrix {
+    pub fn new(rows : usize, cols : usize) -> Matrix {
         let mut data = Vec::with_capacity(rows);
 
         for _ in 0..rows {
-            data.push(Vec::with_capacity(cols));
+            let mut row = Vec::with_capacity(cols);
+            for _ in 0..cols {
+                row.push(0);
+            }
+            data.push(row);
         }
 
         Matrix { data }
     }
 
-    fn new_with_data(init_data : Vec<Vec<u8>>) -> Matrix {
+    pub fn new_with_data(init_data : Vec<Vec<u8>>) -> Matrix {
         let rows = init_data.len();
         let cols = init_data[0].len();
 
@@ -30,8 +34,24 @@ impl Matrix {
 
         Matrix { data }
     }
-}
 
-fn dummy() {
-    galois::add(1, 2);
+    pub fn identity(size : usize) -> Matrix {
+        Self::new(size, size)
+    }
+
+    pub fn columns_count(&self) -> usize {
+        self.data[0].len()
+    }
+
+    pub fn rows_count(&self) -> usize {
+        self.data.len()
+    }
+
+    pub fn get(&self, r : usize, c : usize) -> u8 {
+        self.data[r][c]
+    }
+
+    pub fn set(&mut self, r : usize, c : usize, val : u8) {
+        self.data[r][c] = val;
+    }
 }
