e9731172eb491867105ae71932431225206da8b9
Darren Ldl <darrenldldev@gmail.com>
2017-12-14 14:28:08 +1100

    Updated test code

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
 extern crate mpmc_rb;
 
 use mpmc_rb::channel;
-use std::thread;
+use std::{thread, time};
+use std::time::SystemTime;
 
 fn main() {
-    let (tx, rx) = channel::<String>(1);
+    let (tx, rx) = channel::<String>(1000);
 
     let mut v = vec![];
 
-    for i in 1..2 {
+    for i in 1..5 {
         let tx = tx.clone();
         let handle = thread::spawn(move || {
             loop {
-                let msg = String::from("abcd");
+                let msg = format!("{:?}", SystemTime::now());
                 println!("Sender {}, sending {}", i, msg);
                 tx.send(msg).unwrap();
+                thread::sleep(time::Duration::from_millis(10));
             }
         });
         v.push(handle);
     }
 
-    for i in 1..5 {
+    for i in 1..10 {
         let rx = rx.clone();
         let handle = thread::spawn(move || {
             loop {
