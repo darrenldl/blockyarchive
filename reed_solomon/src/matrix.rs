fcde5c2dfb428b73091501bcd7e4d10a6cbc720a
Darren Ldl <darrenldldev@gmail.com>
2017-12-15 04:31:11 +1100

    Added construction functions for matrix

--- reed_solomon/src/matrix.rs
+++ reed_solomon/src/matrix.rs
@@ -1,5 +1,37 @@
 use galois;
 
+pub struct Matrix {
+    data : Vec<Vec<u8>>
+}
+
+impl Matrix {
+    fn new(rows : usize, cols : usize) -> Matrix {
+        let mut data = Vec::with_capacity(rows);
+
+        for _ in 0..rows {
+            data.push(Vec::with_capacity(cols));
+        }
+
+        Matrix { data }
+    }
+
+    fn new_with_data(init_data : Vec<Vec<u8>>) -> Matrix {
+        let rows = init_data.len();
+        let cols = init_data[0].len();
+
+        let mut data = Vec::with_capacity(rows);
+
+        for r in init_data.into_iter() {
+            if r.len() != cols {
+                panic!("Inconsistent row sizes")
+            }
+            data.push(r);
+        }
+
+        Matrix { data }
+    }
+}
+
 fn dummy() {
     galois::add(1, 2);
 }
