62231645ddd16fe8b321bd63f99a6e0c03c6869f
Darren Ldl <darrenldldev@gmail.com>
2017-12-15 13:02:45 +1100

    Progress on ReedSolomon code

--- reed_solomon/src/lib.rs
+++ reed_solomon/src/lib.rs
@@ -2,6 +2,47 @@
 mod galois;
 mod matrix;
 
+use matrix::Matrix;
+
+struct ReedSolomon {
+    data_shard_count   : usize,
+    parity_shard_count : usize,
+    total_shard_count  : usize,
+    matrix             : Matrix,
+    parity_rows        : Vec<Vec<u8>>,
+}
+
+impl ReedSolomon {
+    fn build_matrix(data_shards : usize, total_shards : usize) -> Matrix {
+        let vandermonde = Matrix::vandermonde(data_shards, total_shards);
+
+        let top = vandermonde.sub_matrix(0, 0, data_shards, data_shards);
+
+        vandermonde.multiply(&top.invert())
+    }
+
+    pub fn new(data_shards : usize, parity_shards : usize) -> ReedSolomon {
+        if 256 < data_shards + parity_shards {
+            panic!("Too many shards, max is 256")
+        }
+
+        let total_shards = data_shards + parity_shards;
+        let matrix       = Self::build_matrix(data_shards, total_shards);
+        let mut parity_rows  = Vec::with_capacity(parity_shards);
+        for i in 0..parity_shards {
+            parity_rows.push(matrix.get_row(data_shards + i));
+        }
+
+        ReedSolomon {
+            data_shard_count   : data_shards,
+            parity_shard_count : parity_shards,
+            total_shard_count  : total_shards,
+            matrix,
+            parity_rows
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
 }
