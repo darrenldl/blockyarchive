d3657c8c485bf9f94face80b437d6bb1723ddeb8
Darren Ldl <darrenldldev@gmail.com>
2017-12-18 05:05:17 +1100

    Added missing multihash test cases

--- src/multihash.rs
+++ src/multihash.rs
@@ -296,6 +296,13 @@ mod test_vectors {
     }
 
     #[test]
+    fn multihash_sha256_bytes() {
+        test_single_multihash(HashType::SHA256,
+                              Raw(""),
+                              Hex("1220e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"));
+    }
+
+    #[test]
     fn sha256_test_vectors() {
         test_single_vector(HashType::SHA256,
                            Raw("abc"),
@@ -313,6 +320,13 @@ mod test_vectors {
                         Raw("a"),
                         1_000_000,
                         Hex("cdc76e5c9914fb9281a1c7e284d73e67f1809a48a497200e046d39ccc7112cd0"));
+    }
+
+    #[test]
+    fn multihash_sha512_bytes() {
+        test_single_multihash(HashType::SHA512,
+                              Raw(""),
+                              Hex("1340cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e"));
     }
 
     #[test]
@@ -341,6 +355,13 @@ mod test_vectors {
                            Hex(""),
                            Hex(""));
     }*/
+
+    #[test]
+    fn multihash_blake2b_512_bytes() {
+        test_single_multihash(HashType::BLAKE2B_512,
+                              Hex(""),
+                              Hex("b24040786a02f742015903c6c6fd852552d272912f4740e15847618a86e217f71f5419d25e1031afee585313896444934eb04b903a685b1448b755d56f701afe9be2ce"));
+    }
 
     #[test]
     fn blake2b_512_test_vectors() {
