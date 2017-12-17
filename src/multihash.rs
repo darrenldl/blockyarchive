2a53436ad68fb1d10d985bbd914dc845e9cd19a5
Darren Ldl <darrenldldev@gmail.com>
2017-12-17 14:24:26 +1100

    Test suite overhaul

--- src/multihash.rs
+++ src/multihash.rs
@@ -161,24 +161,119 @@ pub mod hash {
 
 #[cfg(test)]
 mod test_vectors {
-    // test vectors copied from :
-    // https://www.di-mgt.com.au/sha_testvectors.html
+    // SHA1, SHA256, SHA512 test vectors are copied from : https://www.di-mgt.com.au/sha_testvectors.html
+    // BLAKE2B_256 test vectors are copied from : https://github.com/froydnj/ironclad/blob/master/testing/test-vectors/blake2-256.testvec
+    // BLAKE2B_512 test vectors are copied from : https://github.com/froydnj/ironclad/blob/master/testing/test-vectors/blake2.testvec
 
     use super::super::misc_utils;
     use super::*;
 
+    enum Data<'a> {
+        Raw(&'a str),
+        Hex(&'a str)
+    }
+
+    use self::Data::{Raw, Hex};
+    use std::string::String;
+
+    fn test_single_vector(hash_type : HashType, input : Data, expect : Data) {
+        let input  : Box<[u8]> = match input {
+            Raw(x) => x.to_string().into_bytes().into_boxed_slice(),
+            Hex(x) => misc_utils::hex_string_to_bytes(x).unwrap()
+        };
+        let expect : Box<[u8]> = match expect {
+            Raw(x) => x.to_string().into_bytes().into_boxed_slice(),
+            Hex(x) => misc_utils::hex_string_to_bytes(x).unwrap()
+        };
+        let mut ctx = hash::Ctx::new(hash_type).unwrap();
+        ctx.update(input.as_ref());
+        let result = ctx.finish_into_bytes();
+
+        assert_eq!(expect, result);
+    }
+
+    fn test_repetition(hash_type : HashType, input : Data, repeat : usize, expect : Data) {
+        let input  = match input {
+            Raw(x) => x.to_string().into_bytes().into_boxed_slice(),
+            Hex(x) => misc_utils::hex_string_to_bytes(x).unwrap()
+        };
+        let expect = match expect {
+            Raw(x) => x.to_string().into_bytes().into_boxed_slice(),
+            Hex(x) => misc_utils::hex_string_to_bytes(x).unwrap()
+        };
+        let mut ctx = hash::Ctx::new(hash_type).unwrap();
+        for _ in 0..repeat {
+            ctx.update(input.as_ref());
+        }
+        let result = ctx.finish_into_bytes();
+
+        assert_eq!(expect, result);
+    }
+
     #[test]
     fn sha1_test_vectors() {
-        {
-            let input  =
-                "abc".as_bytes();
-            let expect =
-                misc_utils::hex_string_to_bytes("a9993e364706816aba3e25717850c26c9cd0d89d").unwrap();
-            let mut ctx = hash::Ctx::new(HashType::SHA1).unwrap();
-            ctx.update(input.as_ref());
-            let result = ctx.finish_into_bytes();
+        test_single_vector(HashType::SHA1,
+                           Raw("abc"),
+                           Hex("a9993e364706816aba3e25717850c26c9cd0d89d"));
+        test_single_vector(HashType::SHA1,
+                           Raw(""),
+                           Hex("da39a3ee5e6b4b0d3255bfef95601890afd80709"));
+        test_single_vector(HashType::SHA1,
+                           Raw("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"),
+                           Hex("84983e441c3bd26ebaae4aa1f95129e5e54670f1"));
+        test_single_vector(HashType::SHA1,
+                           Raw("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu"),
+                           Hex("a49b2446a02c645bf419f995b67091253a04a259"));
+        test_repetition(HashType::SHA1,
+                        Raw("a"),
+                        1_000_000,
+                        Hex("34aa973cd4c4daa4f61eeb2bdbad27316534016f"));
+    }
 
-            assert_eq!(expect, result);
-        }
+    #[test]
+    fn sha256_test_vectors() {
+        test_single_vector(HashType::SHA256,
+                           Raw("abc"),
+                           Hex("ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"));
+        test_single_vector(HashType::SHA256,
+                           Raw(""),
+                           Hex("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"));
+        test_single_vector(HashType::SHA256,
+                           Raw("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"),
+                           Hex("248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1"));
+        test_single_vector(HashType::SHA256,
+                           Raw("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu"),
+                           Hex("cf5b16a778af8380036ce59e7b0492370b249b11e8f07a51afac45037afee9d1"));
+        test_repetition(HashType::SHA256,
+                        Raw("a"),
+                        1_000_000,
+                        Hex("cdc76e5c9914fb9281a1c7e284d73e67f1809a48a497200e046d39ccc7112cd0"));
+    }
+
+    #[test]
+    fn sha512_test_vectors() {
+        test_single_vector(HashType::SHA512,
+                           Raw("abc"),
+                           Hex("ddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f"));
+        test_single_vector(HashType::SHA512,
+                           Raw(""),
+                           Hex("cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e"));
+        test_single_vector(HashType::SHA512,
+                           Raw("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"),
+                           Hex("204a8fc6dda82f0a0ced7beb8e08a41657c16ef468b228a8279be331a703c33596fd15c13b1b07f9aa1d3bea57789ca031ad85c7a71dd70354ec631238ca3445"));
+        test_single_vector(HashType::SHA512,
+                           Raw("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu"),
+                           Hex("8e959b75dae313da8cf4f72814fc143f8f7779c6eb9f7fa17299aeadb6889018501d289e4900f7e4331b99dec4b5433ac7d329eeb6dd26545e96e55b874be909"));
+        test_repetition(HashType::SHA512,
+                        Raw("a"),
+                        1_000_000,
+                        Hex("e718483d0ce769644e2e42c7bc15b4638e1f98b13b2044285632a803afa973ebde0ff244877ea60a4cb0432ce577c31beb009c5c2c49aa2e4eadb217ad8cc09b"));
+    }
+
+    #[test]
+    fn blake2b_256_test_vectors() {
+        test_single_vector(HashType::BLAKE2B_256,
+                           Raw("The lazy fox jumps over the lazy dog."),
+                           Hex("0e5751c026e543b2e8ab2eb06099daa1d1e5df47778f7787faab45cdf12fe3a8"));
     }
 }
