8056457cf011032765cb4984268d3c23642940ab
Darren Ldl <darrenldldev@gmail.com>
2018-01-07 20:53:56 +0800

    Added test suite

--- src/sbx_block/header_test.rs
+++ src/sbx_block/header_test.rs
@@ -8,10 +8,12 @@ fn test_from_bytes() {
     let mut header = Header::new(Version::V1,
                              [0; 6]);
 
-    let buffer : &[u8; 16] = b"SBx\x01\xCD\xEF\x03\x03\x03\x03\x03\x03\x00\x00\x00\x00";
+    let buffer : &[u8; 16] = b"SBx\x01\xCD\xEF\x00\x01\x02\x03\x04\x05\x01\x02\x03\x04";
 
     header.from_bytes(buffer).unwrap();
 
     assert_eq!(header.version, Version::V1);
     assert_eq!(header.crc, 0xCDEF);
+    assert_eq!(header.file_uid, *b"\x00\x01\x02\x03\x04\x05");
+    assert_eq!(header.seq_num, 0x01020304);
 }
