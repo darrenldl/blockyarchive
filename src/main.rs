9e0b184e74011f65d728e8a590c524f8002d0767
Darren Ldl <darrenldldev@gmail.com>
2018-01-09 14:13:56 +0800

    Added missing mod declaration

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,13 @@ mod rand_utils;
 mod sbx_block;
 mod sbx_specs;
 
+mod encode_core;
+mod decode_core;
+mod rescue_core;
+mod repair_core;
+mod show_core;
+mod sort_core;
+
 #[macro_use]
 extern crate nom;
 
