358224b0a29a781f2a19f230429eaf92543de511
Darren Ldl <darrenldldev@gmail.com>
2018-01-23 00:08:32 +0800

    Added Error display code

--- src/file_error.rs
+++ src/file_error.rs
@@ -22,7 +22,24 @@ impl fmt::Display for FileError {
     fn fmt(&self, f : &mut fmt::Formatter) -> fmt::Result {
         use self::ErrorKind::*;
         match self.kind {
-            NotFound => write!(f, " file : {} not found", self.path),
+            NotFound          => write!(f, "file : {} not found", self.path),
+            PermissionDenied  => write!(f, "file : {} permission denied", self.path),
+            ConnectionRefused => panic!("Invalid error"),
+            ConnectionReset   => panic!("Invalid error"),
+            ConnectionAborted => panic!("Invalid error"),
+            NotConnected      => panic!("Invalid error"),
+            AddrInUse         => panic!("Invalid error"),
+            AddrNotAvailable  => panic!("Invalid error"),
+            BrokenPipe        => panic!("Invalid error"),
+            AlreadyExists     => panic!("Invalid error"),
+            WouldBlock        => panic!("Invalid error"),
+            InvalidInput      => panic!("Invalid parameters"),
+            InvalidData       => panic!("Invalid parameters"),
+            TimedOut          => write!(f, "file : {} operation timed out", &self.path),
+            WriteZero         => write!(f, "file : {} failed write", &self.path),
+            Interrupted       => write!(f, "file : {} operation interrupted", &self.path),
+            Other             => write!(f, "file : {} unknown error", &self.path),
+            UnexpectedEof     => write!(f, "file : {} unexpected EOF", &self.path),
             _        => write!(f, "Unknown error")
         }
     }
