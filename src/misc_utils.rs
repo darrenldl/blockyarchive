3212a0191afacc695cfbb5f9a3e3d2de4f0191e0
Darren Ldl <darrenldldev@gmail.com>
2018-01-23 20:09:27 +0800

    Removed extraneous println

--- src/misc_utils.rs
+++ src/misc_utils.rs
@@ -37,7 +37,6 @@ pub fn hex_string_to_bytes(string : &str) -> Result<Box<[u8]>, Error> {
             let l_chr_val = hex_char_to_value(string[i]);
             let r_chr_val = hex_char_to_value(string[i+1]);
 
-            println!("{}, {}", l_chr_val, r_chr_val);
             result.push(l_chr_val * 0x10 + r_chr_val);
         }
         Ok(result.into_boxed_slice())
