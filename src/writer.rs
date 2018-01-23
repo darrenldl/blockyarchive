d232e4c45a9101299ed8126524560afa95b3ac2c
Darren Ldl <darrenldldev@gmail.com>
2018-01-23 13:32:39 +0800

    Started work on writer

--- src/writer.rs
+++ src/writer.rs
@@ -1,1 +1,21 @@
 use super::FileError;
+use std::io::Write;
+use std::fs::File;
+
+pub struct Writer {
+    file : File,
+    path : String,
+}
+
+impl Writer {
+    pub fn new(path : String) -> Result<Writer, FileError> {
+        let file = match File::create(&path) {
+            Ok(f) => f,
+            Err(e) => { return Err(FileError::new(e.kind(), path)); }
+        };
+        Ok (Writer {
+            file,
+            path
+        })
+    }
+}
