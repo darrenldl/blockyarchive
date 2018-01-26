d906a882a8ea574adad021c13e93a1a52e30b2a3
Darren Ldl <darrenldldev@gmail.com>
2018-01-26 10:54:38 +0800

    Updated macros

--- src/main.rs
+++ src/main.rs
@@ -16,59 +16,62 @@ use reed_solomon_erasure::ReedSolomon;
 
 macro_rules! worker_stop {
     (
-        graceful => $tx_error:path, $shutdown_flag:path
+        graceful => $tx_error:path, $shutdown_flag:path $([$( $x:path ),*]),*
     ) => {{
         use std::sync::atomic::Ordering;
         $tx_error.send(None).unwrap();
+        $( $x.send(None).unwrap(); )*
         $shutdown_flag.store(true, Ordering::Relaxed);
         break;
     }};
     (
-        graceful_ret => $tx_error:path, $shutdown_flag:path
+        graceful_ret => $tx_error:path, $shutdown_flag:path $([$( $x:path ),*]),*
     ) => {{
         use std::sync::atomic::Ordering;
         $tx_error.send(None).unwrap();
+        $( $x.send(None).unwrap(); )*
         $shutdown_flag.store(true, Ordering::Relaxed);
         return;
     }};
     (
-        graceful_if ($cond:expr) =>$tx_error:path, $shutdown_flag:path
+        graceful_if ($cond:expr) =>$tx_error:path, $shutdown_flag:path $([$( $x:path ),*]),*
     ) => {{
         if $cond {
-            worker_stop!(graceful => $tx_error, $shutdown_flag)
+            worker_stop!(graceful => $tx_error, $shutdown_flag $([$( $x ),*]),*)
         }
     }};
     (
-        graceful_if_ret ($cond:expr) =>$tx_error:path, $shutdown_flag:path
+        graceful_if_ret ($cond:expr) =>$tx_error:path, $shutdown_flag:path $([$( $x:path ),*]),*
     ) => {{
         if $cond {
-            worker_stop!(graceful_ret => $tx_error, $shutdown_flag)
+            worker_stop!(graceful_ret => $tx_error, $shutdown_flag $([$( $x ),*]),*)
         }
     }};
     (
-        graceful_if_shutdown => $tx_error:path, $shutdown_flag:path
+        graceful_if_shutdown => $tx_error:path, $shutdown_flag:path $([$( $x:path ),*]),*
     ) => {{
         use std::sync::atomic::Ordering;
         worker_stop!(graceful_if ($shutdown_flag.load(Ordering::Relaxed)) =>
-                     $tx_error, $shutdown_flag)
+                     $tx_error, $shutdown_flag $([$( $x ),*]),*)
     }};
     (
-        graceful_if_shutdown_ret => $tx_error:path, $shutdown_flag:path
+        graceful_if_shutdown_ret => $tx_error:path, $shutdown_flag:path $([$( $x:path ),*]),*
     ) => {{
         use std::sync::atomic::Ordering;
         worker_stop!(graceful_if_ret ($shutdown_flag.load(Ordering::Relaxed)) =>
-                     $tx_error, $shutdown_flag)
+                     $tx_error, $shutdown_flag $([$( $x ),*]),*)
     }};
     (
-        with_error $error:expr => $tx_error:path, $shutdown_flag:path
+        with_error $error:expr => $tx_error:path, $shutdown_flag:path $([$( $x:path ),*]),*
     ) => {{
         use std::sync::atomic::Ordering;
         $tx_error.send(Some($error)).unwrap();
+        $( $x.send(None).unwrap(); )*
         $shutdown_flag.store(true, Ordering::Relaxed);
         break;
     }};
     (
-        with_error_if_fail ($expr:expr) => $tx_error:path, $shutdown_flag:path
+        with_error_if_fail ($expr:expr) => $tx_error:path, $shutdown_flag:path $([$( $x:path ),*]),*
     ) => {{
         match $expr {
             Ok(res) => res,
@@ -76,10 +79,11 @@ macro_rules! worker_stop {
         }
     }};
     (
-        with_error_ret $error:expr => $tx_error:path, $shutdown_flag:path
+        with_error_ret $error:expr => $tx_error:path, $shutdown_flag:path $([$( $x:path ),*]),*
     ) => {{
         use std::sync::atomic::Ordering;
         $tx_error.send(Some($error)).unwrap();
+        $( $x.send(None).unwrap(); )*
         $shutdown_flag.store(true, Ordering::Relaxed);
         return;
     }}
@@ -87,23 +91,23 @@ macro_rules! worker_stop {
 
 macro_rules! send {
     (
-        no_back_off $item:expr => $sender:ident, $tx_error:path, $shutdown_flag:path
+        no_back_off $item:expr => $sender:ident, $tx_error:path, $shutdown_flag:path $([$( $x:path ),*]),*
     ) => {{
         match $sender.send($item) {
             Ok(()) => {},
-            Err(_) => worker_stop!(graceful => $tx_error, $shutdown_flag)
+            Err(_) => worker_stop!(graceful => $tx_error, $shutdown_flag $([$( $x ),*]),*)
         }
     }};
     (
-        no_back_off_ret $item:expr => $sender:ident, $tx_error:path, $shutdown_flag:path
+        no_back_off_ret $item:expr => $sender:ident, $tx_error:path, $shutdown_flag:path $([$( $x:path ),*]),*
     ) => {{
         match $sender.send($item) {
             Ok(()) => {},
-            Err(_) => worker_stop!(graceful_ret => $tx_error, $shutdown_flag)
+            Err(_) => worker_stop!(graceful_ret => $tx_error, $shutdown_flag $([$( $x ),*]),*)
         }
     }};
     (
-        back_off_millis $time:expr, $item:expr => $sender:ident, $tx_error:path, $shutdown_flag:path
+        back_off_millis $time:expr, $item:expr => $sender:ident, $tx_error:path, $shutdown_flag:path $([$( $x:path ),*]),*
     ) => {{
         use std::time::Duration;
         use std::sync::mpsc::TrySendError;
@@ -111,51 +115,51 @@ macro_rules! send {
             Ok(()) => None,
             Err(TrySendError::Full(b)) => {
                 thread::sleep(Duration::from_millis($time));
-                worker_stop!(graceful_if_shutdown => $tx_error, $shutdown_flag);
+                worker_stop!(graceful_if_shutdown => $tx_error, $shutdown_flag $([$( $x ),*]),*);
                 Some(b)
             },
             Err(TrySendError::Disconnected(_)) =>
-                worker_stop!(graceful => $tx_error, $shutdown_flag)
+                worker_stop!(graceful => $tx_error, $shutdown_flag $([$( $x ),*]),*)
         }
     }};
     (
-        back_off $item:expr => $sender:ident, $tx_error:path, $shutdown_flag:path
+        back_off $item:expr => $sender:ident, $tx_error:path, $shutdown_flag:path $([$( $x:path ),*]),*
     ) => {{
-        send!(back_off_millis 100, $item => $sender, $tx_error, $shutdown_flag)
+        send!(back_off_millis 100, $item => $sender, $tx_error, $shutdown_flag $([$( $x ),*]),*)
     }}
 }
 
 macro_rules! recv {
     (
-        no_timeout => $receiver:ident, $tx_error:path, $shutdown_flag:path
+        no_timeout => $receiver:ident, $tx_error:path, $shutdown_flag:path $([$( $x:path ),*]),*
     ) => {{
         match $receiver.recv() {
             Ok(item) => item,
-            Err(_)   => worker_stop!(graceful => $tx_error, $shutdown_flag)
+            Err(_)   => worker_stop!(graceful => $tx_error, $shutdown_flag $([$( $x ),*]),*)
         }
     }};
     (
-        timeout_millis $timeout:expr => $receiver:ident, $tx_error:path, $shutdown_flag:path
+        timeout_millis $timeout:expr => $receiver:ident, $tx_error:path, $shutdown_flag:path $([$( $x:path ),*]),*
     ) => {{
         use std::time::Duration;
         use std::sync::mpsc::RecvTimeoutError;
         match $receiver.recv_timeout(Duration::from_millis($timeout)) {
             Ok(item)                            => item,
             Err(RecvTimeoutError::Timeout)      => {
-                worker_stop!(graceful_if_shutdown => $tx_error, $shutdown_flag);
+                worker_stop!(graceful_if_shutdown => $tx_error, $shutdown_flag $([$( $x ),*]),*);
                 continue;
             },
             Err(RecvTimeoutError::Disconnected) => {
                 worker_stop!(graceful =>
-                             $tx_error, $shutdown_flag );
+                             $tx_error, $shutdown_flag $([$( $x ),*]),*);
             }
         }
     }};
     (
-        timeout => $receiver:ident, $tx_error:path, $shutdown_flag:path
+        timeout => $receiver:ident, $tx_error:path, $shutdown_flag:path $([$( $x:path ),*]),*
     ) => {{
-        recv!(timeout_millis 1000 => $receiver, $tx_error, $shutdown_flag)
-    }}
+        recv!(timeout_millis 1000 => $receiver, $tx_error, $shutdown_flag $([$( $x ),*]),*)
+    }};
 }
 
 mod file_error;
