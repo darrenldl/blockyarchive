e17a91db733418fc930d2f0ad0b4eb0e90477043
Darren Ldl <darrenldldev@gmail.com>
2018-02-05 21:06:54 +0800

    Code refactoring

--- src/rs_codec/encoder.rs
+++ src/rs_codec/encoder.rs
@@ -7,10 +7,11 @@ use super::super::sbx_specs::ver_to_block_size;
 use super::*;
 
 pub struct RSEncoder {
+    active                    : bool,
     cur_data_index            : u64,
     last_data_set_start_index : u64,
-    rs_codec_normal           : Option<ReedSolomon>,
-    rs_codec_last             : Option<ReedSolomon>,
+    rs_codec_normal           : ReedSolomon,
+    rs_codec_last             : ReedSolomon,
     dat_num_normal            : usize,
     par_num_normal            : usize,
     dat_num_last              : usize,
@@ -44,15 +45,13 @@ impl RSEncoder {
             smallvec![smallvec![0; block_size]; last_data_set_parity_count];
 
         RSEncoder {
+            active : total_data_chunks == 0,
             cur_data_index            : 0,
             last_data_set_start_index,
-            rs_codec_normal :
-            if total_data_chunks == 0 { None }
-            else { Some(ReedSolomon::new(data_shards, parity_shards).unwrap()) },
-            rs_codec_last   :
-            if total_data_chunks == 0 { None }
-            else { Some(ReedSolomon::new(last_data_set_size,
-                                         last_data_set_parity_count).unwrap()) },
+            rs_codec_normal : ReedSolomon::new(data_shards,
+                                               parity_shards).unwrap(),
+            rs_codec_last   : ReedSolomon::new(last_data_set_size,
+                                               last_data_set_parity_count).unwrap(),
             dat_num_normal : data_shards,
             par_num_normal : parity_shards,
             dat_num_last   : last_data_set_size,
@@ -68,75 +67,70 @@ impl RSEncoder {
         self.cur_data_index < self.last_data_set_start_index as u64
     }
 
-    fn add_cur_data_index(&mut self,
-                          val : u64) {
-        self.cur_data_index = self.cur_data_index.wrapping_add(val) % self.total_data_chunks;
+    fn pick_asset_and_incre_cur_data_index(&mut self)
+                                           ->
+        (&ReedSolomon,
+         &mut SmallVec<[SmallVec<[u8; SBX_LARGEST_BLOCK_SIZE]>; 32]>,
+         u64)
+    {
+        let cur_data_index = self.add1_cur_data_index();
+
+        if self.in_normal_data_set() {
+            (&self.rs_codec_normal,
+             &mut self.par_buf_normal,
+             cur_data_index)
+        } else {
+            (&self.rs_codec_last,
+             &mut self.par_buf_last,
+             cur_data_index)
+        }
     }
 
-    fn add1_cur_data_index(&mut self) {
-        self.add_cur_data_index(1);
+    fn add_cur_data_index(&mut self,
+                          val   : u64) -> u64 {
+        let ret = self.cur_data_index;
+
+        self.cur_data_index = self.cur_data_index.wrapping_add(val) % self.total_data_chunks;
+
+        ret
+    }
+
+    fn add1_cur_data_index(&mut self) -> u64 {
+        self.add_cur_data_index(1)
     }
 
     pub fn encode(&mut self,
                   data : &[u8])
                   -> Option<&mut SmallVec<[SmallVec<[u8; SBX_LARGEST_BLOCK_SIZE]>; 32]>> {
+        if !self.active { return None; }
+
         let data = sbx_block::slice_data_buf(self.version, data);
 
-        if self.in_normal_data_set() {
-            let index = (self.cur_data_index % self.dat_num_normal as u64) as usize;
+        let version = self.version;
 
-            {
-                let rs_codec = match self.rs_codec_normal {
-                    None        => { return None; },
-                    Some(ref r) => r,
-                };
-                {
-                    let mut parity : SmallVec<[&mut [u8]; 32]> = SmallVec::with_capacity(self.par_num_normal);
+        let (rs_codec, par_buf, cur_data_index) =
+            self.pick_asset_and_incre_cur_data_index();
 
-                    for p in &mut self.par_buf_normal[0..self.par_num_normal] {
-                        parity.push(sbx_block::slice_data_buf_mut(self.version, p));
-                    }
-                    rs_codec.encode_single_sep(index,
-                                               data,
-                                               &mut parity).unwrap();
-                }
+        let data_shards = rs_codec.data_shard_count();
+
+        let index = (cur_data_index
+                     % rs_codec.data_shard_count() as u64) as usize;
+
+        {
+            let mut parity : SmallVec<[&mut [u8]; 32]> = SmallVec::with_capacity(par_buf.len());
+
+            for p in par_buf.iter_mut() {
+                parity.push(sbx_block::slice_data_buf_mut(version, p));
             }
+            rs_codec.encode_single_sep(index,
+                                       data,
+                                       &mut parity).unwrap();
+        }
 
-            self.add1_cur_data_index();
-
-            if index == self.dat_num_normal - 1 {
-                Some(&mut self.par_buf_normal)
-            } else {
-                None
-            }
+        if index == data_shards - 1 {
+            Some(par_buf)
         } else {
-            let index =
-                (self.cur_data_index - self.last_data_set_start_index as u64) as usize;
-
-            {
-                let rs_codec = match self.rs_codec_last {
-                    None        => { return None; },
-                    Some(ref r) => r
-                };
-                {
-                    let mut parity : SmallVec<[&mut [u8]; 32]> = SmallVec::new();
-
-                    for p in &mut self.par_buf_last[0..self.par_num_last] {
-                        parity.push(sbx_block::slice_data_buf_mut(self.version, p));
-                    }
-                    rs_codec.encode_single_sep(index,
-                                               data,
-                                               &mut parity).unwrap();
-                }
-            }
-
-            self.add1_cur_data_index();
-
-            if index == self.dat_num_last - 1 {
-                Some(&mut self.par_buf_last)
-            } else {
-                None
-            }
+            None
         }
     }
 }
