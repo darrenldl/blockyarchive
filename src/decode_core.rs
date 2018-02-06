a99c792bae2cc988c7121f56077512c3435f04d3
Darren Ldl <darrenldldev@gmail.com>
2018-02-07 02:25:50 +0800

    Began work on decoder core

--- src/decode_core.rs
+++ src/decode_core.rs
@@ -1,4 +1,4 @@
-/*use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex};
 use std::fs;
 use std::fmt;
 use super::file_utils;
@@ -45,4 +45,67 @@ pub struct Stats {
     start_time                  : f64,
     end_time                    : f64,
 }
-*/
+
+impl fmt::Display for Stats {
+    fn fmt(&self, f : &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "")
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Param {
+    in_file  : String,
+    out_file : String,
+    silence_level : progress_report::SilenceLevel
+}
+
+impl Param {
+    pub fn new(in_file  : &str,
+               out_file : &str,
+               silence_level : progress_report::SilenceLevel) -> Param {
+        Param {
+            in_file  : String::from(in_file),
+            out_file : String::from(out_file),
+            silence_level,
+        }
+    }
+}
+
+impl Stats {
+    pub fn new(ref_block     : Block,
+               param         : &Param,
+               file_metadata : &fs::Metadata) -> Stats {
+        let total_blocks =
+            file_utils::calc_total_block_count(ref_block.version);
+        Stats {
+            version                 : ref_block.version,
+            meta_block_decoded      : 0,
+            meta_par_blocks_decoded : 0,
+            data_blocks_decoded     : 0,
+            data_par_blocks_decoded : 0,
+            total_blocks,
+            start_time              : 0.,
+            end_time                : 0.,
+        }
+    }
+}
+
+impl ProgressReport for Stats {
+    fn start_time_mut(&mut self) -> &mut f64 { &mut self.start_time }
+
+    fn end_time_mut(&mut self)   -> &mut f64 { &mut self.end_time }
+
+    fn units_so_far(&self)       -> u64      {
+        (self.meta_blocks_decoded
+         + self.meta_par_blocks_decoded
+         + self.data_blocks_decoded
+         + self.data_par_blocks_decoded) as u64
+    }
+
+    fn total_units(&self)        -> u64      { self.total_blocks as u64 }
+}
+
+fn get_ref_block(in_file : &str) -> Result<Block, Error> {
+}
+
+fn 
--- src/file_utils.rs
+++ src/file_utils.rs
@@ -2,6 +2,7 @@ use std::fs;
 use super::Error;
 use super::sbx_specs::Version;
 use super::sbx_specs::ver_to_data_size;
+use super::sbx_specs::ver_to_block_size;
 
 use super::file_reader::FileReader;
 
@@ -14,4 +15,10 @@ pub fn calc_data_chunk_count(version  : Version,
                              metadata : &fs::Metadata) -> u64 {
     let data_size = ver_to_data_size(version) as u64;
     ((metadata.len() + (data_size - 1)) / data_size)
+}
+
+pub fn calc_total_block_count(version  : Version,
+                              metadata : &fs::Metadata) -> u64 {
+    let block_size = ver_to_block_size(version) as u64;
+    ((metadata.len() + (block_size - 1)) / block_size)
 }
