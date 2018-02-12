4b9ac0c2a72df6f50d516c43cb64d9d6f86e9f53
Darren Ldl <darrenldldev@gmail.com>
2018-02-13 02:21:34 +0800

    Added is_parity

--- src/sbx_block/mod.rs
+++ src/sbx_block/mod.rs
@@ -250,6 +250,24 @@ impl Block {
         }
     }
 
+    pub fn is_parity(&self,
+                     data_shards   : usize,
+                     parity_shards : usize,
+                     total_blocks  : u64) -> bool {
+        match self.block_type() {
+            BlockType::Meta => 0 < self.get_seq_num(),
+            BlockType::Data => {
+                let index =
+                    self.get_seq_num()
+                    - ver_first_data_seq_num(self.get_version());
+                let index_in_set =
+                    index % (data_shards + parity_shards) as u64;
+
+                (data_shards <= index_in_set)
+            }
+        }
+    }
+
     pub fn get_meta_ref_by_id(&self,
                               id : MetadataID)
                               -> Result<Option<&Metadata>, Error> {
