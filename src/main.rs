b6249f294a7819aa2a81a0634a2d90ca45e24b82
Darren Ldl <darrenldldev@gmail.com>
2018-02-26 21:00:12 +1100

    Progress on CLI stuff

--- src/main.rs
+++ src/main.rs
@@ -101,7 +101,7 @@ DIR/INFILE.sbx."))
                          .short("f")
                          .long("force")
                          .help("Force overwrite even if OUT exists"))
-                    .arg(Arg::with_name("hash")
+                    .arg(Arg::with_name("HASH")
                          .long("hash")
                          .takes_value(true)
                          .help("Hash function to use, one of (case-insensitive) :
@@ -113,7 +113,7 @@ DIR/INFILE.sbx."))
                          .long("no-meta")
                          .help("Skip metadata block in the sbx container. Metadata block is
 never skipped for version 11, 12, 13."))
-                    .arg(Arg::with_name("silent")
+                    .arg(Arg::with_name("SILENCE-LEVEL")
                          .short("s")
                          .long("silent")
                          .takes_value(true)
@@ -133,7 +133,7 @@ This only affects progress text printing."))
     12 (bs=128  bytes, Reed-Solomon enabled)
     13 (bs=4096 bytes, Reed-Solomon enabled)
 where bs=sbx block size."))
-                    .arg(Arg::with_name("uid")
+                    .arg(Arg::with_name("UID-HEX")
                          .long("uid")
                          .takes_value(true)
                          .help("Alternative file uid in hex (by default uid is randomly generated).
@@ -160,7 +160,7 @@ OUT is provided and is not a directory, then it is used directly."))
                          .help("Use first whatever valid block as reference block. Use this when
 the container does not have metadata block or when you are okay
 with using a data block as reference block."))
-                    .arg(Arg::with_name("silent")
+                    .arg(Arg::with_name("SILENCE-LEVEL")
                          .short("s")
                          .long("silent")
                          .takes_value(true)
@@ -172,6 +172,41 @@ This only affects progress text printing."))
         )
         .subcommand(SubCommand::with_name("rescue")
                     .about("Rescue sbx blocks from file/block device")
+                    .arg(Arg::with_name("force-misalign")
+                         .long("force-misalign")
+                         .help("Disable automatic rounding down of FROM-BYTE. This is not normally
+used and is only intended for data recovery or related purposes."))
+                    .arg(Arg::with_name("BLOCK-TYPE")
+                         .long("only-pick-block")
+                         .takes_value(true)
+                         .help("Only pick BLOCK-TYPE of blocks, one of :
+    any
+    meta
+    data"))
+                    .arg(Arg::with_name("UID-HEX")
+                         .long("only-pick-uid")
+                         .takes_value(true)
+                         .help("Only pick blocks with UID-HEX as uid. Uid must be exactly 6
+bytes(12 hex digits) in length."))
+                    .arg(Arg::with_name("SILENCE-LEVEL")
+                         .short("s")
+                         .long("silent")
+                         .takes_value(true)
+                         .help("One of :
+    0 (show everything)
+    1 (only show progress stats when done)
+    2 (show nothing)
+This only affects progress text printing."))
+                    .arg(Arg::with_name("FROM-BYTE")
+                         .long("from")
+                         .takes_value(true)
+                         .help("Start from byte FROM-BYTE. The position is automatically rounded
+down to the closest multiple of 128 bytes, after adding the bytes
+processed field from the log file(if specified). If this option is
+not specified, defaults to the start of file. Negative values are
+treated as 0. If FROM-BYTE exceeds the largest possible
+position(file size - 1), then it will be treated as (file size - 1).
+The rounding procedure is applied after all auto-adjustments."))
         )
         .subcommand(SubCommand::with_name("repair")
                     .about("Repair sbx container")
