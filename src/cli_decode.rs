0dad84a8d02517e64397775463efc678a9b43cc8
Darren Ldl <darrenldldev@gmail.com>
2018-03-01 21:32:45 +1100

    Code refactoring

---
+++ src/cli_decode.rs
@@ -0,0 +1,46 @@
+use super::clap::ArgMatches;
+use super::encode_core::Param;
+use super::sbx_specs::{SBX_FILE_UID_LEN,
+                       Version,
+                       string_to_ver,
+                       ver_supports_rs,
+                       ver_to_usize};
+use std::str::FromStr;
+use std::path::Path;
+
+use super::*;
+
+pub fn sub_command<'a, 'b>() -> App<'a, 'b> {
+    SubCommand::with_name("decode")
+        .about("Decode file")
+        .arg(Arg::with_name("in_file")
+             .value_name("INFILE")
+             .required(true)
+             .index(1)
+             .help("Sbx container to decode"))
+        .arg(Arg::with_name("OUT")
+             .index(2)
+             .help("Decoded file name. If OUT is not provided, then name stored in sbx
+container is used if present. If OUT is provided and is a
+directory(DIR) then output file is stored as DIR/STORED_NAME. If
+OUT is provided and is not a directory, then it is used directly."))
+        .arg(Arg::with_name("force")
+             .short("f")
+             .long("force")
+             .help("Force overwrite even if OUT exists"))
+        .arg(Arg::with_name("no_meta")
+             .long("no-meta")
+             .help("Use first whatever valid block as reference block. Use this when
+the container does not have metadata block or when you are okay
+with using a data block as reference block."))
+        .arg(Arg::with_name("silence_level")
+             .value_name("LEVEL")
+             .short("s")
+             .long("silent")
+             .takes_value(true)
+             .help("One of :
+    0 (show everything)
+    1 (only show progress stats when done)
+    2 (show nothing)
+This only affects progress text printing."))
+}
