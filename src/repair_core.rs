d2ea5f92f15334bd890e929d102a0fc9e4eaaecc
Darren Ldl <darrenldldev@gmail.com>
2018-03-11 01:31:39 +1100

    Working on repair core

--- src/repair_core.rs
+++ src/repair_core.rs
@@ -34,3 +34,81 @@ use std::str::from_utf8;
 
 use super::time_utils;
 use super::block_utils;
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Stats {
+    version                        : Version,
+    pub meta_blocks_decoded        : u64,
+    pub data_or_par_blocks_decoded : u64,
+    pub blocks_decode_failed       : u64,
+    total_blocks                   : u64,
+    start_time                     : f64,
+    end_time                       : f64,
+}
+
+impl Stats {
+    pub fn new(ref_block     : &Block,
+               file_metadata : &fs::Metadata) -> Stats {
+        let total_blocks =
+            file_utils::calc_total_block_count(ref_block.get_version(),
+                                               file_metadata);
+        Stats {
+            version                 : ref_block.get_version(),
+            blocks_decode_failed    : 0,
+            meta_blocks_decoded        : 0,
+            data_or_par_blocks_decoded : 0,
+            total_blocks,
+            start_time              : 0.,
+            end_time                : 0.,
+        }
+    }
+}
+
+impl ProgressReport for Stats {
+    fn start_time_mut(&mut self) -> &mut f64 { &mut self.start_time }
+
+    fn end_time_mut(&mut self)   -> &mut f64 { &mut self.end_time }
+
+    fn units_so_far(&self)       -> u64      {
+        (self.meta_blocks_decoded
+         + self.data_or_par_blocks_decoded
+         + self.blocks_decode_failed) as u64
+    }
+
+    fn total_units(&self)        -> u64      { self.total_blocks as u64 }
+}
+
+impl fmt::Display for Stats {
+    fn fmt(&self, f : &mut fmt::Formatter) -> fmt::Result {
+        let block_size             = ver_to_block_size(self.version);
+        let time_elapsed           = (self.end_time - self.start_time) as i64;
+        let (hour, minute, second) = time_utils::seconds_to_hms(time_elapsed);
+
+        writeln!(f, "SBX version                        : {}", ver_to_usize(self.version))?;
+        writeln!(f, "Block size used in checking        : {}", block_size)?;
+        writeln!(f, "Number of blocks processed         : {}", self.units_so_far())?;
+        writeln!(f, "Number of blocks sorted (metadata) : {}", self.meta_blocks_decoded)?;
+        writeln!(f, "Number of blocks sorted (data)     : {}", self.data_or_par_blocks_decoded)?;
+        writeln!(f, "Number of blocks failed to sort    : {}", self.blocks_decode_failed)?;
+        writeln!(f, "Time elapsed                       : {:02}:{:02}:{:02}", hour, minute, second)?;
+
+        Ok(())
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Param {
+    in_file       : String,
+    silence_level : SilenceLevel
+}
+
+impl Param {
+    pub fn new(in_file       : &str,
+               silence_level : SilenceLevel) -> Param {
+        Param {
+            in_file : String::from(in_file),
+            silence_level,
+        }
+    }
+}
+
