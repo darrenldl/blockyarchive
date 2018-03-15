acbdb0e1df6289f1e53fb1910da683a69a01cba7
Darren Ldl <darrenldldev@gmail.com>
2018-03-16 01:37:28 +1100

    Updated versus.sh

--- tests/versus.sh
+++ tests/versus.sh
@@ -4,18 +4,18 @@ uid=$(cat /dev/urandom | tr -dc 0-9a-f | fold -w 12 | head -n 1)
 
 dd if=/dev/urandom of=dummy bs=2096 count=1 &>/dev/null
 
-rm dummy_official.sbx
+rm dummy_official.sbx &>/dev/null
 
-python3 SeqBox/sbxenc.py -uid $uid dummy dummy_official.sbx -o &> /dev/null
+python3 SeqBox/sbxenc.py -uid $uid dummy dummy_official.sbx -o &>/dev/null
 
-rm dummy_osbx.sbx
+rm dummy_rsbx.sbx &>/dev/null
 
-./osbx encode dummy dummy_osbx.sbx -f --uid $uid -s 1 &> /dev/null
+./rsbx encode dummy dummy_rsbx.sbx -f --uid $uid &>/dev/null
 
-cmp -i 512 dummy_official.sbx dummy_osbx.sbx
+cmp -i 512 dummy_official.sbx dummy_rsbx.sbx
 
 if [[ $? == 0 ]]; then
-  echo "No mismatches found"
+  echo "==> Okay"
 else
-  echo "Mismatches detected"
+  echo "==> NOT okay"
 fi
