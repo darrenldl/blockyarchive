a17c4cfa24902afc57931be88715e423a09f0014
Darren Ldl <darrenldldev@gmail.com>
2018-03-16 01:04:49 +1100

    Updated dev_test.sh

--- tests/dev_test.sh
+++ tests/dev_test.sh
@@ -2,61 +2,46 @@
 
 ./copy.sh
 
-cd tests
-
 echo "Generating test data"
-dd if=/dev/urandom of=dummy bs=$[1024 * 1024 * 1] count=1
+dd if=/dev/urandom of=dummy bs=$[1024 * 1024 * 1] count=1 &>/dev/null
 # truncate -s 10m dummy
-echo ""
 
 # version tests
+echo "========================================"
 echo "Starting version tests"
 echo "========================================"
 ./version_tests.sh
-echo "========================================"
-
-echo ""
 
 # nometa tests
+echo "========================================"
 echo "Starting nometa tests"
 echo "========================================"
 ./nometa_tests.sh
-echo "========================================"
-
-echo ""
 
 # hash test
+echo "========================================"
 echo "Starting hash tests"
 echo "========================================"
 ./hash_tests.sh
-echo "========================================"
-
-echo ""
 
 # rescue tests
+echo "========================================"
 echo "Starting rescue tests"
 echo "========================================"
 ./rescue_tests.sh
+
 echo "========================================"
-
-echo ""
-
 echo "Starting rescue with specified range tests"
 echo "========================================"
 ./rescue_from_to_tests.sh
+
 echo "========================================"
-
-echo ""
-
 echo "Starting rescue with specified uid tests"
 echo "========================================"
 ./rescue_pick_uid_tests.sh
-echo "========================================"
-
-echo ""
 
 # output file tests
+echo "========================================"
 echo "Starting output file path logic tests"
 echo "========================================"
 ./out_file_logic_tests.sh
-echo "========================================"
