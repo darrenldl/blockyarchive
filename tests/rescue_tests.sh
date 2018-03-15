07bae9b4ee6e313788fdc3f4fce02401fc34693d
Darren Ldl <darrenldldev@gmail.com>
2018-03-16 00:34:46 +1100

    Updated rescue_tests.sh

--- tests/rescue_tests.sh
+++ tests/rescue_tests.sh
@@ -1,48 +1,52 @@
 #!/bin/bash
 
-VERSIONS=(1 2 3)
+VERSIONS=(1 2 3 17 18 19)
 
-# Encode in all 3 versions
+# Encode in all 6 versions
 for ver in ${VERSIONS[*]}; do
   echo "Encoding in version $ver"
-  ./osbx encode --sbx-version $ver -f dummy rescue$ver.sbx
-  echo ""
+  ./rsbx encode --sbx-version $ver -f dummy rescue$ver.sbx \
+         --rs-data 10 --rs-parity 2 &>/dev/null
 done
 
 # Generate random filler data
 echo "Generating random filler data"
-dd if=/dev/urandom of=filler1 bs=10240 count=1
-dd if=/dev/urandom of=filler2 bs=128   count=1
-dd if=/dev/urandom of=filler3 bs=512   count=1
-echo ""
+dd if=/dev/urandom of=filler1 bs=10240 count=1 &>/dev/null
+dd if=/dev/urandom of=filler2 bs=128   count=1 &>/dev/null
+dd if=/dev/urandom of=filler3 bs=512   count=1 &>/dev/null
 
 # String everything together
 echo "Crafting dummy disk file"
-rm dummy_disk
-cat filler1     >> dummy_disk
-cat rescue3.sbx >> dummy_disk
-cat filler2     >> dummy_disk
-cat rescue1.sbx >> dummy_disk
-cat filler3     >> dummy_disk
-cat rescue2.sbx >> dummy_disk
-cat filler2     >> dummy_disk
-cat filler3     >> dummy_disk
-echo ""
+rm dummy_disk &>/dev/null
+cat filler1      >> dummy_disk
+cat rescue3.sbx  >> dummy_disk
+cat filler2      >> dummy_disk
+cat rescue1.sbx  >> dummy_disk
+cat filler3      >> dummy_disk
+cat rescue2.sbx  >> dummy_disk
+cat filler2      >> dummy_disk
+cat filler3      >> dummy_disk
+cat rescue17.sbx >> dummy_disk
+cat filler2      >> dummy_disk
+cat rescue19.sbx >> dummy_disk
+cat filler2      >> dummy_disk
+cat filler3      >> dummy_disk
+cat filler3      >> dummy_disk
+cat rescue18.sbx >> dummy_disk
+cat filler3      >> dummy_disk
 
 # Rescue from the disk
 echo "Rescuing from dummy disk"
-rm -rf rescued_data
-mkdir rescued_data
-rm rescue_log
-./osbx rescue dummy_disk rescued_data rescue_log
-echo ""
+rm -rf rescued_data &>/dev/null
+mkdir rescued_data &>/dev/null
+rm rescue_log &>/dev/null
+./rsbx rescue dummy_disk rescued_data rescue_log &>/dev/null
 
 # Try to decode the rescued data
 echo "Decoding all rescued data"
 FILES=rescued_data/*
 for f in $FILES; do
-  ./osbx decode $f $f.decoded
-  echo ""
+  ./rsbx decode $f $f.decoded &>/dev/null
 done
 
 echo "Comparing decoded data to original"
@@ -51,8 +55,8 @@ for f in $FILES; do
   echo "Comparing file $f to original"
   cmp dummy $f
   if [[ $? == 0 ]]; then
-    echo "No mismatches detected"
+    echo "==> Okay"
   else
-    echo "Mismatch detected"
+    echo "==> NOT okay"
   fi
 done
