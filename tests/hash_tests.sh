8740142ddad5e5c59f7d597d0bf93f60809ca001
Darren Ldl <darrenldldev@gmail.com>
2018-03-16 00:28:39 +1100

    Updated hash_tests.sh

--- tests/hash_tests.sh
+++ tests/hash_tests.sh
@@ -2,18 +2,29 @@
 
 HASHES=("sha1" "sha256" "sha512" "blake2b-512")
 
+# Record the hashes
+a[0]=$(sha1sum   dummy | awk '{print $1}')
+a[1]=$(sha256sum dummy | awk '{print $1}')
+a[2]=$(sha512sum dummy | awk '{print $1}')
+a[3]=$(b2sum     dummy | awk '{print $1}')
+
 # Encode in all 4 hashes
+i=0
 for h in ${HASHES[*]}; do
   echo "Encoding in hash $h"
-  ./rsbx encode --hash $h -f dummy dummy$h.sbx
-  echo ""
+  output=$(./rsbx encode --hash $h -f dummy dummy$h.sbx | grep "${a[$i]}" )
+  if [[ $output == "" ]]; then
+      echo "==> NOT okay"
+  else
+      echo "==> Okay"
+  fi
+  i=$[$i + 1]
 done
 
 # Decode all of them
 for h in ${HASHES[*]}; do
   echo "Decoding hash $h container"
-  ./rsbx decode -f dummy$h.sbx dummy$h
-  echo ""
+  ./rsbx decode -f dummy$h.sbx dummy$h &>/dev/null
 done
 
 # Compare to original file
@@ -21,8 +32,8 @@ for h in ${HASHES[*]}; do
   echo "Comparing decoded hash $h container data to original"
   cmp dummy dummy$h
   if [[ $? == 0 ]]; then
-    echo "No mismatches detected"
+    echo "==> Okay"
   else
-    echo "Mismatch detected"
+    echo "==> NOT okay"
   fi
 done
