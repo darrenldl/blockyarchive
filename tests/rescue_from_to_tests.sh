b3059e9a3ae91af9fe56a073b6a3f2a1411e3f8a
Darren Ldl <darrenldldev@gmail.com>
2018-03-16 00:39:36 +1100

    Updated rescue_from_to_tests.sh

--- tests/rescue_from_to_tests.sh
+++ tests/rescue_from_to_tests.sh
@@ -3,25 +3,21 @@
 echo "Creating empty files"
 touch dummy_empty1
 touch dummy_empty2
-echo ""
 
 echo "Encoding files"
-./osbx encode -f dummy_empty1 --uid DEADBEEF0001
-./osbx encode -f dummy_empty2 --uid DEADBEEF0002
-echo ""
+./rsbx encode -f dummy_empty1 --uid DEADBEEF0001 &>/dev/null
+./rsbx encode -f dummy_empty2 --uid DEADBEEF0002 &>/dev/null
 
 echo "Crafting dummy disk file"
-rm dummy_empty_disk
+rm dummy_empty_disk &>/dev/null
 cat dummy_empty1.sbx >> dummy_empty_disk
 cat dummy_empty2.sbx >> dummy_empty_disk
-echo ""
 
 echo "Rescuing from dummy disk"
-echo ""
 
-echo "Checking that osbx only decodes first block"
-rm rescued_data/DEADBEEF*
-./osbx rescue dummy_empty_disk rescued_data --from 0 --to 511
+echo "Checking that rsbx only decodes first block"
+rm rescued_data/DEADBEEF* &>/dev/null
+./rsbx rescue dummy_empty_disk rescued_data --from 0 --to 511 &>/dev/null
 if [ -f "rescued_data/DEADBEEF0001" ]; then
   echo "===> Okay"
 else
@@ -33,9 +29,9 @@ else
   echo "===> NOT okay"
 fi
 
-echo "Checking that osbx only decodes second block"
-rm rescued_data/DEADBEEF*
-./osbx rescue dummy_empty_disk rescued_data --from 512 --to 512
+echo "Checking that rsbx only decodes second block"
+rm rescued_data/DEADBEEF* &>/dev/null
+./rsbx rescue dummy_empty_disk rescued_data --from 512 --to 512 &>/dev/null
 if [ ! -f "rescued_data/DEADBEEF0001" ]; then
   echo "===> Okay"
 else
@@ -47,9 +43,9 @@ else
   echo "===> NOT okay"
 fi
 
-echo "Checking that osbx decodes both blocks"
-rm rescued_data/DEADBEEF*
-./osbx rescue dummy_empty_disk rescued_data
+echo "Checking that rsbx decodes both blocks"
+rm rescued_data/DEADBEEF* &>/dev/null
+./rsbx rescue dummy_empty_disk rescued_data &>/dev/null
 if [ -f "rescued_data/DEADBEEF0001" ]; then
   echo "===> Okay"
 else
