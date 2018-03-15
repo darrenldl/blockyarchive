0d7ac7651a76b1d6ec825efe8813cb3c6be82f54
Darren Ldl <darrenldldev@gmail.com>
2018-03-15 23:51:54 +1100

    Updated version_tests.sh

--- tests/version_tests.sh
+++ tests/version_tests.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-VERSIONS=(1 2 3)
+VERSIONS=(1 2 3 17 18 19)
 
 # Encode in all 3 versions
 for ver in ${VERSIONS[*]}; do
   echo "Encoding in version $ver"
-  ./rsbx encode --sbx-version $ver -f dummy dummy$ver.sbx
-  echo ""
+  ./rsbx encode --sbx-version $ver -f dummy dummy$ver.sbx \
+         --rs-data 10 --rs-parity 2 &>/dev/null
 done
 
 # Decode all of them
 for ver in ${VERSIONS[*]}; do
   echo "Decoding version $ver container"
-  ./rsbx decode -f dummy$ver.sbx dummy$ver
-  echo ""
+  ./rsbx decode -f dummy$ver.sbx dummy$ver &>/dev/null
 done
 
 # Compare to original file
@@ -21,8 +20,8 @@ for ver in ${VERSIONS[*]}; do
   echo "Comparing decoded version $ver container data to original"
   cmp dummy dummy$ver
   if [[ $? == 0 ]]; then
-    echo "No mismatches detected"
+    echo "==> No mismatches detected"
   else
-    echo "Mismatch detected"
+    echo "==> Mismatch detected"
   fi
 done
