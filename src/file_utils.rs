9f86bbea595046a866bd7f3fa373ff37a1dd3b15
Darren Ldl <darrenldldev@gmail.com>
2018-03-19 18:06:52 +1100

    Finished file size calcuation functions

--- src/file_utils.rs
+++ src/file_utils.rs
@@ -4,12 +4,15 @@ use std::fs;
 use general_error::Error;
 use sbx_specs::{Version,
                 ver_to_data_size,
+                ver_uses_rs,
                 ver_to_block_size};
 
 use file_reader::{FileReader,
                   FileReaderParam};
 
 use std::path::Path;
+
+use sbx_block;
 
 pub fn get_file_metadata(file : &str) -> Result<fs::Metadata, Error> {
     let reader = FileReader::new(file,
@@ -52,25 +55,27 @@ pub mod from_container_metadata {
 
 pub mod from_orig_file_size {
     use super::*;
-    pub fn calc_total_block_count_exc_burst_gaps(version        : Version,
-                                                 data_par_burst : Option<(usize, usize, usize)>,
-                                                 size           : u64)
-                                                 -> u64 {
+
+    pub fn calc_data_block_count_exc_burst_gaps(version        : Version,
+                                                data_par_burst : Option<(usize, usize, usize)>,
+                                                size           : u64)
+                                                -> u64 {
         let chunk_size  = ver_to_data_size(version) as u64;
         let data_chunks = (size + (chunk_size - 1)) / chunk_size;
 
         match data_par_burst {
             None                    => {
-                let meta_block_count = 1;
+                assert!(!ver_uses_rs(version));
+
                 let data_block_count = data_chunks;
 
-                meta_block_count + data_block_count
+                data_block_count
             },
             Some((data, parity, _)) => {
+                assert!( ver_uses_rs(version));
+
                 let data_shards   = data   as u64;
                 let parity_shards = parity as u64;
-
-                let meta_block_count = 1 + parity_shards as u64;
 
                 let data_block_set_size  = data_shards;
                 let data_block_set_count =
@@ -79,8 +84,36 @@ pub mod from_orig_file_size {
                 let encoded_data_block_set_size  = data_shards + parity_shards;
                 let encoded_data_block_set_count = data_block_set_count;
 
-                meta_block_count
-                    + encoded_data_block_set_count * encoded_data_block_set_size
+                encoded_data_block_set_count * encoded_data_block_set_size
+            }
+        }
+    }
+
+    pub fn calc_total_block_count_exc_burst_gaps(version        : Version,
+                                                 data_par_burst : Option<(usize, usize, usize)>,
+                                                 size           : u64)
+                                                 -> u64 {
+        let data_block_count =
+            calc_data_block_count_exc_burst_gaps(version,
+                                                 data_par_burst,
+                                                 size);
+
+        match data_par_burst {
+            None                 => {
+                let meta_block_count = 1;
+                let data_block_count =
+                    calc_data_block_count_exc_burst_gaps(version,
+                                                         data_par_burst,
+                                                         size);
+
+                meta_block_count + data_block_count
+            },
+            Some((_, parity, _)) => {
+                let parity_shards = parity as u64;
+
+                let meta_block_count = 1 + parity_shards as u64;
+
+                meta_block_count + data_block_count
             }
         }
     }
@@ -89,7 +122,44 @@ pub mod from_orig_file_size {
                                data_par_burst : Option<(usize, usize, usize)>,
                                size           : u64)
                                -> u64 {
-        0
+        let block_size = ver_to_block_size(version) as u64;
+
+        match data_par_burst {
+            None                        => {
+                let block_count =
+                    calc_total_block_count_exc_burst_gaps(version,
+                                                          data_par_burst,
+                                                          size);
+
+                block_size * block_count
+            },
+            Some((data, parity, burst)) => {
+                let data_block_count =
+                    calc_data_block_count_exc_burst_gaps(version,
+                                                         data_par_burst,
+                                                         size);
+
+                let last_seq_num = (data_block_count - 1) as u32;
+
+                let super_block_set_size = ((data + parity) * burst) as u32;
+
+                let first_seq_num_in_block_set =
+                    (last_seq_num / super_block_set_size) * super_block_set_size;
+
+                let mut last_index = 0;
+                for seq_num in first_seq_num_in_block_set..last_seq_num {
+                    let index =
+                        sbx_block::calc_data_block_write_index(seq_num,
+                                                               data_par_burst);
+
+                    if index > last_index {
+                        last_index = index;
+                    }
+                }
+
+                block_size * (last_index + 1)
+            }
+        }
     }
 }
 
