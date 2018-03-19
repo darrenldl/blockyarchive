66856cf741e876ce5af2561032d832383c046d02
Darren Ldl <darrenldldev@gmail.com>
2018-03-19 22:51:11 +1100

    Fixed burst_corruption_tests.sh to reduce test file sizes

--- tests/burst_corruption_tests.sh
+++ tests/burst_corruption_tests.sh
@@ -5,7 +5,7 @@ exit_code=0
 VERSIONS=(17 18 19)
 
 corrupt() {
-    dd if=/dev/zero of=$3 bs=$2 count=1 seek=$1 conv=notrunc &>/dev/null
+    dd if=/dev/zero of=$3 bs=1 count=$2 seek=$1 conv=notrunc &>/dev/null
 }
 
 file_size=$[1024 * 1024 * 1]
