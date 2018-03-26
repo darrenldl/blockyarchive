dd45b40421c2ae9770f78dc536348dd4e64d6acc
Darren Ldl <darrenldldev@gmail.com>
2018-03-26 12:48:43 +1100

    cov.sh fixes

--- cov.sh
+++ cov.sh
@@ -9,3 +9,9 @@ for file in target/debug/rsbx-*; do
   mkdir -p "target/cov/rsbx"
   kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/rsbx" "$file"
 done
+
+for file in ${files[@]}; do
+  # mkdir -p "target/cov/$(basename $file)"
+  mkdir -p "target/cov/rsbx"
+  kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/rsbx" "$file"
+done
