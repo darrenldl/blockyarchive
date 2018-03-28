450826284b5da020ba0ed798cf09a99a70bff9a0
Darren Ldl <darrenldldev@gmail.com>
2018-03-29 05:45:26 +1100

    More tests

--- src/file_utils_tests.rs
+++ src/file_utils_tests.rs
@@ -497,3 +497,13 @@ mod from_orig_file_size {
         }
     }
 }
+
+#[test]
+fn test_get_file_name_part_of_path_simple_cases() {
+    assert_eq!("abcd", get_file_name_part_of_path("test/abcd"));
+    assert_eq!("test.sbx", get_file_name_part_of_path("test/test.sbx"));
+    assert_eq!("abcd", get_file_name_part_of_path("/root/test/abcd"));
+    assert_eq!("abcd_defg.sbx", get_file_name_part_of_path("/root/test/abcd_defg.sbx"));
+    assert_eq!("abcd", get_file_name_part_of_path("abcd"));
+    assert_eq!("test", get_file_name_part_of_path("/test"));
+}
