93862067a016caa5b65a2e6028bcd9060624d011
Darren Ldl <darrenldldev@gmail.com>
2018-03-29 18:45:31 +1100

    Started work on encoder and repairer tests

--- src/rs_codec/mod.rs
+++ src/rs_codec/mod.rs
@@ -15,9 +15,11 @@ macro_rules! assert_ready {
 }
 
 mod encoder;
+mod encoder_tests;
 pub use self::encoder::RSEncoder;
 
 mod repairer;
+mod repairer_tests;
 pub use self::repairer::RSRepairer;
 pub use self::repairer::RSRepairStats;
 
