93773e22b454b5a4824898e81094b7e80c69061f
Darren Ldl <darrenldldev@gmail.com>
2018-03-31 06:05:12 +1100

    Added QuickCheck tests for rs_codec::encoder

--- src/rs_codec/encoder_tests.rs
+++ src/rs_codec/encoder_tests.rs
@@ -76,3 +76,78 @@ fn test_encoder_encode_correctly_simple_cases() {
         }
     }
 }
+
+quickcheck! {
+    fn qc_encoder_encode_same_as_encode(data   : usize,
+                                        parity : usize,
+                                        reuse  : usize) -> bool {
+        let data   = 1 + data % 10;
+        let mut parity = 1 + parity % 10;
+
+        let reuse = reuse % 10;
+
+        let r = ReedSolomon::new(data, parity).unwrap();
+
+        let mut buffer = make_random_block_buffers!(SBX_LARGEST_BLOCK_SIZE, data + parity);
+        let mut buffer_copy = buffer.clone();
+
+        let versions = vec![Version::V17, Version::V18, Version::V19];
+
+        for version in versions.into_iter () {
+            let mut encoder = RSEncoder::new(version, data, parity);
+
+            {
+                let mut refs = Vec::new();
+                for b in buffer.iter_mut() {
+                    refs.push(sbx_block::slice_data_buf_mut(version, b));
+                }
+
+                r.encode(&mut refs).unwrap();
+            }
+
+            for _ in 0..1 + reuse {
+                {
+                    let mut refs = Vec::new();
+                    for b in buffer_copy.iter_mut() {
+                        refs.push(b);
+                    }
+
+                    for i in 0..data - 1 {
+                        if encoder.unfilled_slot_count() != data - i {
+                            return false;
+                        }
+
+                        if !(encoder.encode_no_block_sync(refs[i]) == None
+                             && encoder.active())
+                        {
+                            return false;
+                        }
+                    }
+
+                    if encoder.unfilled_slot_count() != 1 {
+                        return false;
+                    }
+
+                    let mut i = 0;
+                    for b in encoder.encode_no_block_sync(refs[data - 1]).unwrap().iter() {
+                        if sbx_block::slice_data_buf(version, b)
+                            != sbx_block::slice_data_buf(version, &buffer[i + data])
+                        {
+                            return false;
+                        }
+                        i += 1;
+                    }
+                }
+
+                if !(!encoder.active()
+                     && encoder.unfilled_slot_count() == data
+                     && encoder.total_slot_count() == data)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
