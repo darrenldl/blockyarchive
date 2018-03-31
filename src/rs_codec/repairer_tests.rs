8322aad888aeb79154d4860097487a5646e7994c
Darren Ldl <darrenldldev@gmail.com>
2018-04-01 00:39:48 +1100

    Added tests for failure case

--- src/rs_codec/repairer_tests.rs
+++ src/rs_codec/repairer_tests.rs
@@ -148,4 +148,65 @@ fn test_repairer_repair_properly_simple_cases() {
             assert_eq!(Version::V17, block.get_version());
         }
     }
+
+    for _ in 0..2 {
+        {
+            let mut refs = Vec::new();
+            for b in buffer.iter_mut() {
+                refs.push(sbx_block::slice_buf(version, b));
+            }
+
+            // mark 0, 2, 3, 11 as missing
+            let mut i = 0;
+            for b in refs.iter() {
+                assert_eq!(13 - i, repairer.unfilled_slot_count());
+                assert_eq!(13, repairer.total_slot_count());
+
+                repairer.get_block_buffer().copy_from_slice(b);
+                let codec_state =
+                    if i == 0 || i == 2 || i == 3 || i == 11 {
+                        repairer.mark_missing()
+                    } else {
+                        repairer.mark_present()
+                    };
+
+                assert_eq!(13 - i - 1, repairer.unfilled_slot_count());
+                assert_eq!(13, repairer.total_slot_count());
+
+                if i == 12 {
+                    assert_eq!(RSCodecState::Ready, codec_state);
+                } else {
+                    assert_eq!(RSCodecState::NotReady, codec_state);
+                }
+
+                i += 1;
+            }
+        }
+
+        let (stats, blocks) = repairer.repair_with_block_sync(1);
+
+        assert_eq!(Version::V17, stats.version);
+        assert_eq!((10, 3, 0), stats.data_par_burst);
+        assert!(!stats.successful);
+        assert_eq!(1, stats.start_seq_num);
+        assert_eq!(false, stats.present[0]);
+        assert_eq!(true,  stats.present[1]);
+        assert_eq!(false, stats.present[2]);
+        assert_eq!(false, stats.present[3]);
+        assert_eq!(true,  stats.present[4]);
+        assert_eq!(true,  stats.present[5]);
+        assert_eq!(true,  stats.present[6]);
+        assert_eq!(true,  stats.present[7]);
+        assert_eq!(true,  stats.present[8]);
+        assert_eq!(true,  stats.present[9]);
+        assert_eq!(true,  stats.present[10]);
+        assert_eq!(false, stats.present[11]);
+        assert_eq!(true,  stats.present[12]);
+
+        assert_eq!(13, stats.present.len());
+        assert_eq!(4, stats.missing_count);
+        assert_eq!(9, stats.present_count);
+
+        assert_eq!(0, blocks.len());
+    }
 }
