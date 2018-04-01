ed2c0c468ca38a03b51e6028e57cf8737cea8c4d
Darren Ldl <darrenldldev@gmail.com>
2018-04-01 18:08:04 +1000

    Still fixing

--- cov_tests/kcov_rsbx_fun.sh
+++ cov_tests/kcov_rsbx_fun.sh
@@ -10,8 +10,8 @@ mkdir -p "$HOME/target/cov/rsbx"
 
 function kcov_rsbx() {
     if [[ $TRAVIS == true ]]; then
-        kcov --exclude-pattern=/.cargo,/usr/lib --verify "$HOME/target/cov/rsbx" rsbx "$@"
-        kcov --coveralls-id=$TRAVIS_JOB_ID --exclude-pattern=/.cargo,/usr/lib --verify "$HOME/target/cov/rsbx" rsbx "$@"
+        kcov --exclude-pattern=/.cargo,/usr/lib --verify "$HOME/target/cov/rsbx" rsbx "$@" &>/dev/null
+        kcov --coveralls-id=$TRAVIS_JOB_ID --exclude-pattern=/.cargo,/usr/lib --verify "$HOME/target/cov/rsbx" rsbx "$@" &>/dev/null
         ./rsbx "$@"
     else
         kcov --exclude-pattern=/.cargo,/usr/lib --verify "../target/cov/rsbx" rsbx "$@"
