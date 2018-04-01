d68689ac03dca59478e22b1f617353f0dbfd6e34
Darren Ldl <darrenldldev@gmail.com>
2018-04-01 13:42:54 +1000

    Fixing kcov script

--- cov_tests/kcov_rsbx_fun.sh
+++ cov_tests/kcov_rsbx_fun.sh
@@ -6,6 +6,8 @@ if [[ $TRAVIS == true ]]; then
     export PATH=$TARGET/bin:$PATH
 fi
 
+mkdir -p "target/cov/rsbx"
+
 function kcov_rsbx() {
     kcov --exclude-pattern=/.cargo,/usr/lib --verify "../target/cov/rsbx" rsbx "$@"
 }
