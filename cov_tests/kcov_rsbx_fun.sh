488b34751ad2081be7bf84bc7904d18622829baa
Darren Ldl <darrenldldev@gmail.com>
2018-04-02 04:02:35 +1000

    Trying

--- cov_tests/kcov_rsbx_fun.sh
+++ cov_tests/kcov_rsbx_fun.sh
@@ -11,7 +11,7 @@ kcov_rsbx() {
     echo "$PWD"
     if [[ $TRAVIS == true ]]; then
         # kcov --coveralls-id=$TRAVIS_JOB_ID --exclude-pattern=/.cargo,/usr/lib --verify "../target/cov/rsbx" rsbx "$@" &>/dev/null
-        kcov --exclude-pattern=/.cargo,/usr/lib "../target/cov/rsbx" ./rsbx "$@"
+        kcov --verify "../target/cov/rsbx" ./rsbx "$@"
         #./rsbx "$@"
     else
         kcov --exclude-pattern=/.cargo,/usr/lib --verify "../target/cov/rsbx" ./rsbx "$@"
