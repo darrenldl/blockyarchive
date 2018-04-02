92125c7cc28c32982fb65dbd185bef465b930c72
Darren Ldl <darrenldldev@gmail.com>
2018-04-02 15:31:28 +1000

    Reverted debugging lines

--- cov_tests/kcov_rsbx_fun.sh
+++ cov_tests/kcov_rsbx_fun.sh
@@ -8,11 +8,9 @@ fi
 
 kcov_rsbx() {
     mkdir -p "../target/cov/rsbx"
-    echo "$PWD"
     if [[ $TRAVIS == true ]]; then
-        # kcov --coveralls-id=$TRAVIS_JOB_ID --exclude-pattern=/.cargo,/usr/lib --verify "../target/cov/rsbx" rsbx "$@" &>/dev/null
+        kcov --coveralls-id=$TRAVIS_JOB_ID --exclude-pattern=/.cargo,/usr/lib --verify "../target/cov/rsbx" rsbx "$@" &>/dev/null
         kcov --verify "../target/cov/rsbx" ./rsbx "$@"
-        #./rsbx "$@"
     else
         kcov --exclude-pattern=/.cargo,/usr/lib --verify "../target/cov/rsbx" ./rsbx "$@"
     fi
--- cov_tests/version_tests.sh
+++ cov_tests/version_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# source kcov_rsbx_fun.sh
+source kcov_rsbx_fun.sh
 
 exit_code=0
 
@@ -14,27 +14,17 @@ fi
 # VERSIONS=(1 2 3 17 18 19)
 VERSIONS=(1)
 
-mkdir -p "../target/cov/rsbx"
-
-echo $(which kcov)
-echo $PWD
-
 # Encode in all 6 versions
 for ver in ${VERSIONS[*]}; do
   echo "Encoding in version $ver"
-  #kcov_rsbx encode --sbx-version $ver -f dummy dummy$ver.sbx \
-  #          --rs-data 10 --rs-parity 2 #&>/dev/null
-  kcov --include-path .. --verify "../target/cov/rsbx" rsbx encode --sbx-version $ver -f dummy dummy$ver.sbx \
-                --rs-data 10 --rs-parity 2 #&>/dev/null
-  # ./rsbx encode --sbx-version $ver -f dummy dummy$ver.sbx \
-  #     --rs-data 10 --rs-parity 2 #&>/dev/null
+  kcov_rsbx encode --sbx-version $ver -f dummy dummy$ver.sbx \
+            --rs-data 10 --rs-parity 2 #&>/dev/null
 done
 
 # Decode all of them
 for ver in ${VERSIONS[*]}; do
   echo "Decoding version $ver container"
-  # kcov_rsbx decode -f dummy$ver.sbx dummy$ver &>/dev/null
-  ./rsbx decode -f dummy$ver.sbx dummy$ver &>/dev/null
+  kcov_rsbx decode -f dummy$ver.sbx dummy$ver &>/dev/null
 done
 
 # Compare to original file
