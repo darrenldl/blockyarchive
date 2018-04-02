12938157f32518e44aca122a96dc7e24f46ea2fc
Darren Ldl <darrenldldev@gmail.com>
2018-04-02 14:21:29 +1000

    Tests

--- cov_tests/version_tests.sh
+++ cov_tests/version_tests.sh
@@ -4,6 +4,10 @@
 
 exit_code=0
 
+if [[ $TRAVIS == true ]]; then
+    export PATH=$TARGET/bin:$PATH
+fi
+
 # VERSIONS=(1 2 3 17 18 19)
 VERSIONS=(1)
 
@@ -14,16 +18,17 @@ for ver in ${VERSIONS[*]}; do
   echo "Encoding in version $ver"
   #kcov_rsbx encode --sbx-version $ver -f dummy dummy$ver.sbx \
   #          --rs-data 10 --rs-parity 2 #&>/dev/null
-  #kcov --verify "../target/cov/rsbx" rsbx encode --sbx-version $ver -f dummy dummy$ver.sbx \
-  #              --rs-data 10 --rs-parity 2 #&>/dev/null
-  ./rsbx encode --sbx-version $ver -f dummy dummy$ver.sbx \
-      --rs-data 10 --rs-parity 2 #&>/dev/null
+  kcov --verify "../target/cov/rsbx" rsbx encode --sbx-version $ver -f dummy dummy$ver.sbx \
+                --rs-data 10 --rs-parity 2 #&>/dev/null
+  # ./rsbx encode --sbx-version $ver -f dummy dummy$ver.sbx \
+  #     --rs-data 10 --rs-parity 2 #&>/dev/null
 done
 
 # Decode all of them
 for ver in ${VERSIONS[*]}; do
   echo "Decoding version $ver container"
-  kcov_rsbx decode -f dummy$ver.sbx dummy$ver &>/dev/null
+  # kcov_rsbx decode -f dummy$ver.sbx dummy$ver &>/dev/null
+  ./rsbx decode -f dummy$ver.sbx dummy$ver &>/dev/null
 done
 
 # Compare to original file
