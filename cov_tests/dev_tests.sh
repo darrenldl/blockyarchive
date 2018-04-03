817048a1cc4949c84082d2aa3a776e377913fe7b
Darren Ldl <darrenldldev@gmail.com>
2018-04-04 05:48:04 +1000

    Fixed condition

--- cov_tests/dev_tests.sh
+++ cov_tests/dev_tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if ! [[ $TARGET == x86_64-unknown-linux-gnu && $DISABLE_COV == "" ]]; then
-    exit 0
+if [[ $TRAVIS == true ]]; then
+    if ! [[ $TARGET == x86_64-unknown-linux-gnu && $DISABLE_COV == "" ]]; then
+        exit 0
+    fi
 fi
 
 cd cov_tests
