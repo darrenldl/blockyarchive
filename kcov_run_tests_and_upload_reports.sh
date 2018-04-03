7de58e3365c7369ab4c474cf3139ae3e0b10de07
Darren Ldl <darrenldldev@gmail.com>
2018-04-03 17:27:19 +1000

    Fixing conditional coverage testing

---
+++ kcov_run_tests_and_upload_reports.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [[ $TARGET != x86_64-unknown-linux-gnu ]]; then
+    exit 0
+fi
+
+export PATH=$HOME/kcov/bin:$PATH
+
+for file in target/debug/rsbx-*; do
+    if [[ $file == *.d ]]; then
+        continue
+    fi
+
+    kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/rsbx" "$file"
+done
+
+wget https://codecov.io/bash -O codecov_uploader
+chmod u+x codecov_uploader
+
+./codecov_uploader -s "target/cov/rsbx"
+
+echo "Uploaded code coverage to Codecov"
+
+for file in target/debug/rsbx-*; do
+    if [[ $file == *.d ]]; then
+        continue
+    fi
+
+    kcov --coveralls-id=$TRAVIS_JOB_ID --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/rsbx" "$file"
+done
+
+echo "Uploaded code coverage to Coveralls"
+
+exit 0
