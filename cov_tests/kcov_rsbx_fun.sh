4e987f0ea0687381a17fbe3bd1ca0ec333be4b12
Darren Ldl <darrenldldev@gmail.com>
2018-04-26 23:52:06 +1000

    Updated cov tests stuff

--- cov_tests/kcov_rsbx_fun.sh
+++ cov_tests/kcov_rsbx_fun.sh
@@ -10,9 +10,8 @@ fi
 
 kcov_rsbx() {
     if [[ $TRAVIS == true ]]; then
-        kcov --coveralls-id=$TRAVIS_JOB_ID --exclude-pattern=/.cargo,/usr/lib --verify "../target/cov/rsbx" rsbx "$@" &>/dev/null
-        ./rsbx "$@"
+        kcov --coveralls-id=$TRAVIS_JOB_ID --exclude-pattern=/.cargo,/usr/lib --verify "../target/cov/rsbx" rsbx "$@" | sed "s/kcov.*//"
     else
-        kcov --exclude-pattern=/.cargo,/usr/lib --verify "../target/cov/rsbx" ./rsbx "$@"
+        kcov --exclude-pattern=/.cargo,/usr/lib --verify "../target/cov/rsbx" ./rsbx "$@" | sed "s/kcov.*//"
     fi
 }
