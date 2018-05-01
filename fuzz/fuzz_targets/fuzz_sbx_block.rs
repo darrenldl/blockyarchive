054d402b5c6f502f5dfe92b7dae3e59f7ef15041
Darren Ldl <darrenldldev@gmail.com>
2018-05-01 13:10:03 +1000

    Updated fuzz suite

--- fuzz/fuzz_targets/fuzz_sbx_block.rs
+++ fuzz/fuzz_targets/fuzz_sbx_block.rs
@@ -1,7 +1,8 @@
 #![no_main]
 #[macro_use] extern crate libfuzzer_sys;
-extern crate rsbx;
+extern crate rsbx_lib;
+
+use rsbx_lib::sbx_block;
 
 fuzz_target!(|data: &[u8]| {
-    // fuzzed code goes here
 });
--- fuzz/fuzz_targets/fuzz_sbx_header.rs
+++
@@ -1,6 +0,0 @@
-#![no_main]
-#[macro_use] extern crate libfuzzer_sys;
-extern crate rsbx_lib;
-
-fuzz_target!(|data: &[u8]| {
-});
