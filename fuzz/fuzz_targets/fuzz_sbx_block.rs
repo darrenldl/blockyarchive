93677162f214b5a7254aac7de40e4439943ff913
Darren Ldl <darrenldldev@gmail.com>
2018-05-03 03:08:02 +1000

    Updated fuzz_sbx_block

--- fuzz/fuzz_targets/fuzz_sbx_block.rs
+++ fuzz/fuzz_targets/fuzz_sbx_block.rs
@@ -3,6 +3,22 @@
 extern crate rsbx_lib;
 
 use rsbx_lib::sbx_block;
+use rsbx_lib::sbx_block::Block;
+
+use rsbx_lib::sbx_specs;
 
 fuzz_target!(|data: &[u8]| {
+    let mut block = Block::dummy();
+
+    let mut buffer : [u8; 4096] = [0; 4096];
+
+    if data.len() > 4096 {
+        if let Ok(()) = block.sync_from_buffer(data, None) {
+            let block_size = sbx_specs::ver_to_block_size(block.get_version());
+
+            block.sync_to_buffer(None, &mut buffer).unwrap();
+
+            assert_eq!(&buffer[0..block_size], &data[0..block_size]);
+        }
+    }
 });
