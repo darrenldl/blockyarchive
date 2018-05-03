fd54281c13cf3fe0c573013a065fef2bbb83ee45
Darren <darrenldl@users.noreply.github.com>
2018-05-03 20:58:04 +1000

    Fixed #7, #77 (#78)
    * Made minimal changes to make the crate to be both lib and bin
    
    * Disabled doc
    
    * Removed unused mod
    
    * Adjusted pub
    
    * Added fuzz code via cargo-fuzz
    
    * Updated fuzz Cargo.toml
    
    * Updated fuzz suite
    
    * Added pub keyword
    
    * Updated fuzz_sbx_block
    
    * Removed unused import in fuzzing code
    
    * Fixed fuzzing code
    
    * Version bump and updated changelog
    
    * Updated fuzz suite
    
    * Fixed fuzzing code
    
    * Added improvements to fuzzing code

---
+++ fuzz/fuzz_targets/fuzz_sbx_block.rs
@@ -0,0 +1,42 @@
+#![no_main]
+#[macro_use] extern crate libfuzzer_sys;
+extern crate rsbx_lib;
+
+use rsbx_lib::sbx_block::Block;
+
+use rsbx_lib::sbx_specs;
+
+fuzz_target!(|data: &[u8]| {
+    let mut block = Block::dummy();
+    let mut block2 = Block::dummy();
+
+    let mut buffer : [u8; 4096] = [0; 4096];
+    let mut buffer2 : [u8; 4096] = [0; 4096];
+
+    if data.len() >= 4096 {
+        if let Ok(()) = block.sync_from_buffer(data, None) {
+            let block_size = sbx_specs::ver_to_block_size(block.get_version());
+
+            buffer.copy_from_slice(&data[0..block_size]);
+            buffer2.copy_from_slice(&data[0..block_size]);
+
+            for i in 0..16 {
+                buffer[i]  = 0;
+                buffer2[i] = 0;
+            }
+
+            block.sync_to_buffer(None, &mut buffer).unwrap();
+
+            block2.sync_from_buffer(&buffer, None).unwrap();
+
+            block2.sync_to_buffer(None, &mut buffer2).unwrap();
+
+            if block.get_seq_num() > 0 {
+                assert_eq!(&buffer[0..block_size], &data[0..block_size]);
+                assert_eq!(&buffer[0..block_size], &buffer2[0..block_size]);
+            }
+
+            assert_eq!(&buffer[0..16], &buffer2[0..16]);
+        }
+    }
+});
---
+++ src/lib.rs
@@ -0,0 +1,116 @@
+#[cfg(test)]
+#[macro_use]
+extern crate quickcheck;
+
+#[macro_use]
+extern crate nom;
+
+extern crate clap;
+
+extern crate rand;
+
+extern crate chrono;
+
+extern crate smallvec;
+extern crate ctrlc;
+
+extern crate reed_solomon_erasure;
+
+extern crate ring;
+extern crate blake2_c;
+
+mod crc_ccitt;
+
+macro_rules! smallvec {
+    [
+        $arr:ty => $val:expr; $len:expr
+    ] => {{
+        let mut v : SmallVec<$arr> = SmallVec::with_capacity($len);
+        for _ in 0..$len {
+            v.push($val);
+        }
+        v
+    }};
+    [
+        $val:expr; $len:expr
+    ] => {{
+        let mut v = SmallVec::with_capacity($len);
+        for _ in 0..$len {
+            v.push($val);
+        }
+        v
+    }}
+}
+
+macro_rules! break_if_eof_seen {
+    (
+        $read_res:expr
+    ) => {
+        if $read_res.eof_seen { break; }
+    }
+}
+
+mod file_error;
+
+mod general_error;
+use general_error::Error;
+use general_error::ErrorKind;
+
+#[macro_use]
+mod json_macros;
+
+#[macro_use]
+mod misc_macros;
+
+#[macro_use]
+mod block_preds;
+
+mod multihash;
+mod multihash_tests;
+pub mod json_printer;
+mod json_utils;
+mod misc_utils;
+mod misc_utils_tests;
+mod file_utils;
+mod file_utils_tests;
+mod rand_utils;
+mod time_utils;
+mod time_utils_tests;
+mod integer_utils;
+mod integer_utils_tests;
+mod block_utils;
+
+pub mod sbx_block;
+pub mod sbx_specs;
+mod sbx_specs_tests;
+
+mod log;
+
+mod rs_codec;
+
+#[macro_use]
+mod cli_macros;
+
+mod encode_core;
+mod decode_core;
+mod rescue_core;
+mod repair_core;
+mod show_core;
+mod sort_core;
+mod check_core;
+
+mod progress_report;
+
+mod file_reader;
+mod file_writer;
+
+mod cli_utils;
+
+pub mod cli_encode;
+pub mod cli_decode;
+pub mod cli_rescue;
+pub mod cli_show;
+pub mod cli_repair;
+pub mod cli_check;
+pub mod cli_sort;
+pub mod cli_calc;
