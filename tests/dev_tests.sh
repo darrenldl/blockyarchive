7b64296877dc5646dc70fa6e1302b0f35b08f5ee
Darren Ldl <darrenldldev@gmail.com>
2018-12-08 16:31:47 +1100

    Moved dummy file generation to a separate script

--- tests/dev_tests.sh
+++ tests/dev_tests.sh
@@ -13,7 +13,7 @@ cd tests
 test_failed=0
 
 echo "Generating test data"
-dd if=/dev/urandom of=dummy bs=$[1024 * 1024 * 1] count=1 &>/dev/null
+./gen_dummy.sh
 # truncate -s 10m dummy
 
 # version tests
---
+++ tests/gen_dummy.sh
@@ -0,0 +1,2 @@
+#!/bib/bash
+dd if=/dev/urandom of=dummy bs=$[1024 * 1024 * 1] count=1 &>/dev/null
