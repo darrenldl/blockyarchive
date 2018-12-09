a661de629dcf35869ccb98301a5fa67a480446d2
Darren Ldl <darrenldldev@gmail.com>
2018-12-09 21:22:45 +1100

    Added version_tests for decode stdout

---
+++ tests/version_tests_decode_stdout.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+exit_code=0
+
+VERSIONS=(1 2 3 17 18 19)
+
+# Encode in all 6 versions
+for ver in ${VERSIONS[*]}; do
+  echo -n "Encoding in version $ver"
+  output=$(./rsbx encode --json --sbx-version $ver -f dummy dummy$ver.sbx \
+                  --rs-data 10 --rs-parity 2)
+  if [[ $(echo $output | jq -r ".error") != null ]]; then
+      echo " ==> Invalid JSON"
+      exit_code=1
+  fi
+  if [[ $(echo $output | jq -r ".stats.sbxVersion") == "$ver" ]]; then
+      echo " ==> Okay"
+  else
+      echo " ==> NOT okay"
+      exit_code=1
+  fi
+done
+
+# Check all of them
+for ver in ${VERSIONS[*]}; do
+    echo -n "Checking version $ver container"
+    output=$(./rsbx check --json --pv 2 --verbose dummy$ver.sbx 2>/dev/null)
+    if [[ $(echo $output | jq -r ".error") != null ]]; then
+        echo " ==> Invalid JSON"
+        exit_code=1
+    fi
+    if [[ $(echo $output | jq -r ".stats.numberOfBlocksFailedCheck") == 0 ]]; then
+        echo " ==> Okay"
+    else
+        echo " ==> NOT okay"
+        exit_code=1
+    fi
+done
+
+# Show all
+for ver in ${VERSIONS[*]}; do
+    echo -n "Checking show output for $ver container"
+    output=$(./rsbx show --json --pv 1 dummy$ver.sbx 2>/dev/null)
+    if [[ $(echo $output | jq -r ".error") != null ]]; then
+        echo " ==> Invalid JSON"
+        exit_code=1
+    fi
+    if [[ $(echo $output | jq -r ".blocks[0].sbxContainerVersion") == $ver ]]; then
+        echo " ==> Okay"
+    else
+        echo " ==> NOT okay"
+        exit_code=1
+    fi
+done
+
+# Decode all of them
+for ver in ${VERSIONS[*]}; do
+  echo -n "Decoding version $ver container"
+  output=$(./rsbx decode --json --verbose dummy$ver.sbx - 2>&1 > dummy$ver)
+  if [[ $(echo $output | jq -r ".error") != null ]]; then
+      echo " ==> Invalid JSON"
+      exit_code=1
+  fi
+  if [[ $(echo $output | jq -r ".stats.sbxVersion") == "$ver" ]]; then
+      echo " ==> Okay"
+  else
+      echo " ==> NOT okay"
+      exit_code=1
+  fi
+done
+
+# Compare to original file
+for ver in ${VERSIONS[*]}; do
+  echo -n "Comparing decoded version $ver container data to original"
+  cmp dummy dummy$ver
+  if [[ $? == 0 ]]; then
+    echo " ==> Okay"
+  else
+    echo " ==> NOT okay"
+    exit_code=1
+  fi
+done
+
+exit $exit_code
