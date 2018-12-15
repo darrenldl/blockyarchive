5ac8d25192ed72625f6e77824118b4a2e6fa3b64
Darren Ldl <darrenldldev@gmail.com>
2018-12-15 15:55:50 +1100

    Added compare_decode_file_and_stdout_nometa to dev_tests

--- tests/dev_tests.sh
+++ tests/dev_tests.sh
@@ -59,7 +59,7 @@ fi
 
 # compare decode file and stdout output
 echo "========================================"
-echo "Starting comparison between decode with file input and stdout output"
+echo "Starting comparison between decode with file output and stdout output"
 echo "========================================"
 ./compare_decode_file_and_stdout.sh
 if [[ $? != 0 ]]; then
@@ -68,7 +68,7 @@ if [[ $? != 0 ]]; then
 fi
 
 echo "========================================"
-echo "Starting comparison between decode with file input and stdout output with corrupted container"
+echo "Starting comparison between decode with file output and stdout output with corrupted container"
 echo "========================================"
 ./compare_decode_file_and_stdout_corrupted_container.sh
 if [[ $? != 0 ]]; then
@@ -102,6 +102,15 @@ echo "========================================"
 if [[ $? != 0 ]]; then
   test_failed=$[$test_failed+1]
   test_failed_names=$test_failed_names"- nometa_tests_decode_stdout.sh\n"
+fi
+
+echo "========================================"
+echo "Starting comparison between decode with file output and stdout output with nometa"
+echo "========================================"
+./compare_decode_file_and_stdout_nometa.sh
+if [[ $? != 0 ]]; then
+  test_failed=$[$test_failed+1]
+  test_failed_names=$test_failed_names"- compare_decode_file_and_stdout_nometa.sh\n"
 fi
 
 # hash test
