c943be8f57a31f5948bd3ec3611faa69b203e2c0
Darren Ldl <darrenldldev@gmail.com>
2018-12-22 01:45:12 +1100

    decode_manual_burst_decode_stdout fix

--- tests/decode_manual_burst_decode_stdout.sh
+++ tests/decode_manual_burst_decode_stdout.sh
@@ -30,12 +30,12 @@ for ver in ${VERSIONS[*]}; do
       exit_code=1
     fi
     if [[ $(echo $output | jq -r ".stats.sbxVersion") == "$ver" ]]; then
-      echo " ==> Okay"
+      echo -n " ==> Okay"
     else
-      echo " ==> NOT okay"
+      echo -n " ==> NOT okay"
       exit_code=1
     fi
-    if [[ $(echo $output | jq -r ".stats.recordedHash") == $(echo $output | jq -r ".stats.hashOfOutputFile") ]]; then
+    if [[ $(echo $output | jq -r ".stats.recordedHash") != $(echo $output | jq -r ".stats.hashOfOutputFile") ]]; then
       echo " ==> Okay"
     else
       echo " ==> NOT okay"
