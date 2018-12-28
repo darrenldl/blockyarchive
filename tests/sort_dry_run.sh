c2594fdf4fde099f03f1e4d3a2bd104d92660e3a
Darren Ldl <darrenldldev@gmail.com>
2018-12-28 20:28:27 +1100

    Added sort_dry_run.sh

---
+++ tests/sort_dry_run.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+exit_code=0
+
+VERSIONS=(1 2 3 17 18 19)
+
+file_size=$[1024 * 1024 * 10]
+
+# generate test data
+dd if=/dev/urandom of=dummy bs=$file_size count=1 &>/dev/null
+
+for ver in ${VERSIONS[*]}; do
+    for (( i=0; i < 3; i++ )); do
+        if   [[ $ver ==  1 ]]; then
+            data_shards=$((1 + RANDOM % 128))
+            parity_shards=$((1 + RANDOM % 128))
+        elif [[ $ver ==  2 ]]; then
+            data_shards=$((1 + RANDOM % 128))
+            parity_shards=$((1 + RANDOM % 128))
+        elif [[ $ver ==  3 ]]; then
+            data_shards=$((1 + RANDOM % 128))
+            parity_shards=$((1 + RANDOM % 128))
+        elif [[ $ver == 17 ]]; then
+            data_shards=$((1 + RANDOM % 128))
+            parity_shards=$((1 + RANDOM % 128))
+        elif [[ $ver == 18 ]]; then
+            data_shards=$((1 + RANDOM % 128))
+            parity_shards=$((1 + RANDOM % 128))
+        else
+            data_shards=$((1 + RANDOM % 128))
+            parity_shards=$((1 + RANDOM % 128))
+        fi
+
+        burst=$((RANDOM % 15))
+
+        container_name=sort_$data_shards\_$parity_shards\_$ver.sbx
+
+        echo -n "Encoding in version $ver, data = $data_shards, parity = $parity_shards"
+        output=$(./blkar encode --json --sbx-version $ver -f dummy $container_name \
+                        --hash sha1 \
+                        --rs-data $data_shards --rs-parity $parity_shards)
+        if [[ $(echo $output | jq -r ".error") != null ]]; then
+            echo " ==> Invalid JSON"
+            exit_code=1
+        fi
+        if [[ $(echo $output | jq -r ".stats.sbxVersion") == "$ver" ]]; then
+            echo -n " ==> Okay"
+        else
+            echo -n " ==> NOT okay"
+            exit_code=1
+        fi
+        if [[ $(echo $output | jq -r ".stats.hash" | awk '{ print $1 }') == "SHA1" ]]; then
+            echo " ==> Okay"
+        else
+            echo " ==> NOT okay"
+            exit_code=1
+        fi
+
+        new_burst=$[$burst+2]
+
+        rm -f sorted_$container_name
+
+        echo -n "Sorting container"
+        output=$(./blkar sort --json --dry-run --burst $new_burst $container_name sorted_$container_name)
+        if [[ $(echo $output | jq -r ".error") != null ]]; then
+            echo " ==> Invalid JSON"
+            exit_code=1
+        fi
+        if [[ $(echo $output | jq -r ".stats.sbxVersion") == "$ver" ]]; then
+            echo -n " ==> Okay"
+        else
+            echo -n " ==> NOT okay"
+            exit_code=1
+        fi
+        if [ ! -f sorted_$container_name ]; then
+          echo " == Okay"
+        else
+          echo " ==> NOT okay"
+          exit_code=1
+        fi
+    done
+done
+
+exit $exit_code
