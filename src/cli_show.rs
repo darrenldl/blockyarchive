97da8ea82a5ab36bc533c758541a945270df13d6
Darren Ldl <darrenldldev@gmail.com>
2018-12-30 03:59:01 +1100

    Code refactoring

--- src/cli_show.rs
+++ src/cli_show.rs
@@ -19,12 +19,7 @@ pub fn sub_command<'a, 'b>() -> App<'a, 'b> {
         .arg(force_misalign_arg())
         .arg(pr_verbosity_level_arg())
         .arg(from_byte_arg()
-             .help("Start from byte FROM-BYTE. The position is automatically rounded
-down to the closest multiple of 128 bytes. If this option is not
-specified, defaults to the start of file. Negative values are rejected.
-If FROM-BYTE exceeds the largest possible position (file size - 1),
-then it will be treated as (file size - 1). The rounding procedure
-is applied after all auto-adjustments."))
+             .help(FROM_BYTE_ARG_HELP_MSG_SCAN))
         .arg(to_byte_inc_arg())
         .arg(to_byte_exc_arg())
         .arg(guess_burst_arg())
