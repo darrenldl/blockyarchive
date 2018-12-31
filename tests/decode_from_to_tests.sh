208caad5e0466a23b7bbf5a50faabf82da307ee1
Darren Ldl <darrenldldev@gmail.com>
2019-01-01 01:47:07 +1100

    Added decode_from_to_tests.sh

---
+++ tests/decode_from_to_tests.sh
@@ -0,0 +1,223 @@
+#!/bin/bash
+
+exit_code=0
+
+corrupt() {
+  dd if=/dev/zero of=$2 bs=1 count=1 seek=$1 conv=notrunc &>/dev/null
+}
+
+echo -n "Encoding"
+output=$(./../blkar encode --json -f dummy --uid DEADBEEF0001 --sbx-version 17 --rs-data 10 --rs-parity 2 --burst 10)
+if [[ $(echo $output | jq -r ".error") != null ]]; then
+    echo " ==> Invalid JSON"
+    exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.fileUID") == "DEADBEEF0001" ]]; then
+    echo " ==> Okay"
+else
+    echo " ==> NOT okay"
+    exit_code=1
+fi
+
+echo "Decoding dummy disk"
+
+echo "Collecting base statistics"
+output=$(./../blkar decode --json -f dummy.sbx)
+if [[ $(echo $output | jq -r ".error") != "null" ]]; then
+  echo " ==> Invalid JSON"
+  exit_code=1
+fi
+blocks_processed=$(echo $output | jq -r ".stats.numberOfBlocksProcessed")
+okay_meta=$(echo $output | jq -r ".stats.numberOfBlocksDecodedMetadata")
+okay_data=$(echo $output | jq -r ".stats.numberOfBlocksDecodedData")
+failed_blocks=$(echo $output | jq -r ".stats.numberOfBlocksFailedToDecode")
+
+echo -n "Checking that blkar only decodes the first block"
+output=$(./../blkar decode --json -f dummy.sbx --from 0 --to-inc 511)
+if [[ $(echo $output | jq -r ".error") != "null" ]]; then
+  echo " ==> Invalid JSON"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksProcessed") == 1 ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksDecodedMetadata") == 1 ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksDecodedData") == 0 ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksFailedToDecode") == 0 ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+
+corrupt 0 dummy.sbx
+
+output=$(./../blkar decode --json -f dummy.sbx --from 0 --to-inc 511)
+if [[ $(echo $output | jq -r ".error") != "null" ]]; then
+  echo " ==> Invalid JSON"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksProcessed") == 1 ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksDecodedMetadata") == 0 ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksDecodedData") == 0 ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksFailedToDecode") == 1 ]]; then
+  echo " ==> Okay"
+else
+  echo " ==> NOT okay"
+  exit_code=1
+fi
+
+echo -n "Checking that blkar only decodes the second block"
+output=$(./../blkar decode --json -f dummy.sbx --from 512 --to-inc 512)
+if [[ $(echo $output | jq -r ".error") != "null" ]]; then
+  echo " ==> Invalid JSON"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksProcessed") == 1 ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksDecodedMetadata") == 0 ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksDecodedData") == 1 ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksFailedToDecode") == 0 ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+
+corrupt 512 dummy.sbx
+
+output=$(./../blkar decode --json -f dummy.sbx --from 512 --to-inc 512)
+if [[ $(echo $output | jq -r ".error") != "null" ]]; then
+  echo " ==> Invalid JSON"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksProcessed") == 1 ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksDecodedMetadata") == 0 ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksDecodedData") == 0 ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksFailedToDecode") == 1 ]]; then
+  echo " ==> Okay"
+else
+  echo " ==> NOT okay"
+  exit_code=1
+fi
+
+echo -n "Checking that blkar decodes both blocks"
+output=$(./../blkar decode --json -f dummy.sbx --from 0 --to-exc 1024)
+if [[ $(echo $output | jq -r ".error") != "null" ]]; then
+  echo " ==> Invalid JSON"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksProcessed") == 2 ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksDecodedMetadata") == 0 ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksDecodedData") == 0 ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksFailedToDecode") == 2 ]]; then
+  echo " ==> Okay"
+else
+  echo " ==> NOT okay"
+  exit_code=1
+fi
+
+echo -n "Checking that blkar decodes all blocks"
+output=$(./../blkar decode --json -f dummy.sbx)
+if [[ $(echo $output | jq -r ".error") != "null" ]]; then
+  echo " ==> Invalid JSON"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksProcessed") == $blocks_processed ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksDecodedMetadata") == $[okay_meta - 1] ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksDecodedData") == $[okay_data - 1] ]]; then
+  echo -n " ==> Okay"
+else
+  echo -n " ==> NOT okay"
+  exit_code=1
+fi
+if [[ $(echo $output | jq -r ".stats.numberOfBlocksFailedToDecode") == $[failed_blocks+2] ]]; then
+  echo " ==> Okay"
+else
+  echo " ==> NOT okay"
+  exit_code=1
+fi
+
+echo $exit_code > exit_code
