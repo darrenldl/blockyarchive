88067a509d652b0ab539694934e9ddf3cb96dd13
Darren Ldl <darrenldldev@gmail.com>
2019-01-07 17:53:32 +1100

    Added decode ref block from to tests

--- tests/dev_tests.sh
+++ tests/dev_tests.sh
@@ -85,8 +85,11 @@ tests=(
   "sort_from_to_tests_rounding"
   "sort_from_to_tests_force_misalign"
   "check_ref_block_from_to_tests"
-  "check_ref_block_from_to_rounding"
-  "check_ref_block_from_to_force_misalign"
+  "check_ref_block_from_to_tests_rounding"
+  "check_ref_block_from_to_tests_force_misalign"
+  "decode_ref_block_from_to_tests"
+  "decode_ref_block_from_to_tests_rounding"
+  "decode_ref_block_from_to_tests_force_misalign"
 )
 
 test_count=${#tests[@]}
