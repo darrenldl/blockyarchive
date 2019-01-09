c74595b3cc377a7dd8c214ba454bb3d3154cedaa
Darren Ldl <darrenldldev@gmail.com>
2019-01-10 02:31:12 +1100

    Reenabled all verions in show_guess_burst_force_misalign

--- tests/show_guess_burst_force_misalign.sh
+++ tests/show_guess_burst_force_misalign.sh
@@ -9,77 +9,76 @@ file_size=$(ls -l dummy | awk '{ print $5 }')
 # generate test data
 dd if=/dev/urandom of=dummy bs=$file_size count=1 &>/dev/null
 
-# for ver in ${VERSIONS[*]}; do
-for ver in 17; do
-    for (( i=0; i < 3; i++ )); do
-        if   [[ $ver ==  1 ]]; then
-            data_shards=$((1 + RANDOM % 128))
-            parity_shards=$((1 + RANDOM % 128))
-        elif [[ $ver ==  2 ]]; then
-            data_shards=$((1 + RANDOM % 128))
-            parity_shards=$((1 + RANDOM % 128))
-        elif [[ $ver ==  3 ]]; then
-            data_shards=$((1 + RANDOM % 128))
-            parity_shards=$((1 + RANDOM % 128))
-        elif [[ $ver == 17 ]]; then
-            data_shards=$((1 + RANDOM % 128))
-            parity_shards=$((1 + RANDOM % 128))
-        elif [[ $ver == 18 ]]; then
-            data_shards=$((1 + RANDOM % 128))
-            parity_shards=$((1 + RANDOM % 128))
-        else
-            data_shards=$((1 + RANDOM % 128))
-            parity_shards=$((1 + RANDOM % 128))
-        fi
+for ver in ${VERSIONS[*]}; do
+  for (( i=0; i < 3; i++ )); do
+    if   [[ $ver ==  1 ]]; then
+      data_shards=$((1 + RANDOM % 128))
+      parity_shards=$((1 + RANDOM % 128))
+    elif [[ $ver ==  2 ]]; then
+      data_shards=$((1 + RANDOM % 128))
+      parity_shards=$((1 + RANDOM % 128))
+    elif [[ $ver ==  3 ]]; then
+      data_shards=$((1 + RANDOM % 128))
+      parity_shards=$((1 + RANDOM % 128))
+    elif [[ $ver == 17 ]]; then
+      data_shards=$((1 + RANDOM % 128))
+      parity_shards=$((1 + RANDOM % 128))
+    elif [[ $ver == 18 ]]; then
+      data_shards=$((1 + RANDOM % 128))
+      parity_shards=$((1 + RANDOM % 128))
+    else
+      data_shards=$((1 + RANDOM % 128))
+      parity_shards=$((1 + RANDOM % 128))
+    fi
 
-        burst=$[1 + RANDOM % 15]
-        offset=$[500 + RANDOM % 1000]
+    burst=$[1 + RANDOM % 15]
+    offset=$[500 + RANDOM % 1000]
 
-        container_name=show_$data_shards\_$parity_shards\_$ver.sbx
+    container_name=show_$data_shards\_$parity_shards\_$ver.sbx
 
-        echo -n "Encoding in version $ver, data = $data_shards, parity = $parity_shards"
-        output=$(./../blkar encode --json --sbx-version $ver -f dummy $container_name \
+    echo -n "Encoding in version $ver, data = $data_shards, parity = $parity_shards"
+    output=$(./../blkar encode --json --sbx-version $ver -f dummy $container_name \
                         --hash sha1 \
                         --rs-data $data_shards --rs-parity $parity_shards \
                         --burst $burst)
-        if [[ $(echo $output | jq -r ".error") != null ]]; then
-            echo " ==> Invalid JSON"
-            exit_code=1
-        fi
-        if [[ $(echo $output | jq -r ".stats.sbxVersion") == "$ver" ]]; then
-            echo -n " ==> Okay"
-        else
-            echo -n " ==> NOT okay"
-            exit_code=1
-        fi
-        if [[ $(echo $output | jq -r ".stats.hash" | awk '{ print $1 }') == "SHA1" ]]; then
-            echo " ==> Okay"
-        else
-            echo " ==> NOT okay"
-            exit_code=1
-        fi
+    if [[ $(echo $output | jq -r ".error") != null ]]; then
+      echo " ==> Invalid JSON"
+      exit_code=1
+    fi
+    if [[ $(echo $output | jq -r ".stats.sbxVersion") == "$ver" ]]; then
+      echo -n " ==> Okay"
+    else
+      echo -n " ==> NOT okay"
+      exit_code=1
+    fi
+    if [[ $(echo $output | jq -r ".stats.hash" | awk '{ print $1 }') == "SHA1" ]]; then
+      echo " ==> Okay"
+    else
+      echo " ==> NOT okay"
+      exit_code=1
+    fi
 
-        mv $container_name $container_name.tmp
-        touch $container_name
-        truncate -s $offset $container_name
-        cat $container_name.tmp >> $container_name
-        rm $container_name.tmp
+    mv $container_name $container_name.tmp
+    touch $container_name
+    truncate -s $offset $container_name
+    cat $container_name.tmp >> $container_name
+    rm $container_name.tmp
 
-        echo -n "Checking sorted container burst error resistance level"
-        output=$(./../blkar show --json --force-misalign --guess-burst-from $offset --from $offset --guess-burst $container_name)
-        if [[ $(echo $output | jq -r ".error") != null ]]; then
-            echo " ==> Invalid JSON"
-            exit_code=1
-        fi
-        burst_shown=$(echo $output | jq -r ".bestGuessForBurstErrorResistanceLevel")
-        if [[ (($ver == "1" || $ver == "2" || $ver == "3") && ($burst_shown == "null"))
-            || (($ver == "17" || $ver == "18" || $ver == "19") && ($burst_shown == $burst)) ]]; then
-            echo " ==> Okay"
-        else
-            echo " ==> NOT okay"
-            exit_code=1
-        fi
-    done
+    echo -n "Checking sorted container burst error resistance level"
+    output=$(./../blkar show --json --force-misalign --guess-burst-from $offset --from $offset --guess-burst $container_name)
+    if [[ $(echo $output | jq -r ".error") != null ]]; then
+      echo " ==> Invalid JSON"
+      exit_code=1
+    fi
+    burst_shown=$(echo $output | jq -r ".bestGuessForBurstErrorResistanceLevel")
+    if [[ (($ver == "1" || $ver == "2" || $ver == "3") && ($burst_shown == "null"))
+              || (($ver == "17" || $ver == "18" || $ver == "19") && ($burst_shown == $burst)) ]]; then
+      echo " ==> Okay"
+    else
+      echo " ==> NOT okay"
+      exit_code=1
+    fi
+  done
 done
 
 echo $exit_code > exit_code
