c357b0998b782497b6dcff1617727a2e8468d5cb
Darren Ldl <darrenldldev@gmail.com>
2019-01-10 02:40:37 +1100

    Upgraded show_guess_burst_force_misalign

--- tests/show_guess_burst_force_misalign.sh
+++ tests/show_guess_burst_force_misalign.sh
@@ -32,6 +32,56 @@ for ver in ${VERSIONS[*]}; do
     fi
 
     burst=$[1 + RANDOM % 15]
+
+    # check that blkar defaults to guessing from start if --guess-burst-from is not specified
+    offset=$[1 + RANDOM % 100]
+
+    container_name=show_$data_shards\_$parity_shards\_$ver.sbx
+
+    echo -n "Encoding in version $ver, data = $data_shards, parity = $parity_shards"
+    output=$(./../blkar encode --json --sbx-version $ver -f dummy $container_name \
+                        --hash sha1 \
+                        --rs-data $data_shards --rs-parity $parity_shards \
+                        --burst $burst)
+    if [[ $(echo $output | jq -r ".error") != null ]]; then
+      echo " ==> Invalid JSON"
+      exit_code=1
+    fi
+    if [[ $(echo $output | jq -r ".stats.sbxVersion") == "$ver" ]]; then
+      echo -n " ==> Okay"
+    else
+      echo -n " ==> NOT okay"
+      exit_code=1
+    fi
+    if [[ $(echo $output | jq -r ".stats.hash" | awk '{ print $1 }') == "SHA1" ]]; then
+      echo " ==> Okay"
+    else
+      echo " ==> NOT okay"
+      exit_code=1
+    fi
+
+    mv $container_name $container_name.tmp
+    touch $container_name
+    truncate -s $offset $container_name
+    cat $container_name.tmp >> $container_name
+    rm $container_name.tmp
+
+    echo -n "Checking sorted container burst error resistance level"
+    output=$(./../blkar show --json --force-misalign --from $offset --guess-burst $container_name)
+    if [[ $(echo $output | jq -r ".error") != null ]]; then
+      echo " ==> Invalid JSON"
+      exit_code=1
+    fi
+    burst_shown=$(echo $output | jq -r ".bestGuessForBurstErrorResistanceLevel")
+    if [[ (($ver == "1" || $ver == "2" || $ver == "3") && ($burst_shown == "null"))
+              || (($ver == "17" || $ver == "18" || $ver == "19") && ($burst_shown == $burst)) ]]; then
+      echo " ==> Okay"
+    else
+      echo " ==> NOT okay"
+      exit_code=1
+    fi
+
+    # check that blkar moves to the specified location if --guess-burst-from is specified
     offset=$[500 + RANDOM % 1000]
 
     container_name=show_$data_shards\_$parity_shards\_$ver.sbx
