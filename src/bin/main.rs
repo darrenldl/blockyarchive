3183e0983fb6a77d1067a5566d138d3b549190d8
Darren Ldl <darrenldldev@gmail.com>
2019-04-20 21:10:41 +1000

    Stitched update mode into core cli code

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,14 +9,15 @@ fn real_main() -> i32 {
         .version(env!("CARGO_PKG_VERSION"))
         .author("Darren Ldl <darrenldldev@gmail.com>")
         .about("Archive with forward error correction and sector level recoverability")
-        .subcommand(cli_encode::sub_command())
+        .subcommand(cli_calc::sub_command())
+        .subcommand(cli_check::sub_command())
         .subcommand(cli_decode::sub_command())
+        .subcommand(cli_encode::sub_command())
+        .subcommand(cli_repair::sub_command())
         .subcommand(cli_rescue::sub_command())
         .subcommand(cli_show::sub_command())
-        .subcommand(cli_repair::sub_command())
-        .subcommand(cli_check::sub_command())
         .subcommand(cli_sort::sub_command())
-        .subcommand(cli_calc::sub_command())
+        .subcommand(cli_update::sub_command())
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("calc") {
