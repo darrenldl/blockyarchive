1cbd94cd187fb1d0728b71e9208600b6e591ff8f
Darren Ldl <darrenldldev@gmail.com>
2019-05-06 15:50:48 +1000

    Fixed corrupt function

--- tests/functions.sh
+++ tests/functions.sh
@@ -5,8 +5,8 @@ corrupt() {
     byte_cur=$(mktemp)
     dd if=$2 of=$byte_orig bs=1 count=1 skip=$1 &>/dev/null
     while true; do
-        dd if=/dev/zero of=$2 bs=1 count=1 seek=$1 conv=notrunc &>/dev/null
-        dd if=$2 of=$byte_cur bs=1 count=1 skip=$1 &>/dev/null
+        dd if=/dev/urandom of=$2        bs=1 count=1 seek=$1 conv=notrunc &>/dev/null
+        dd if=$2           of=$byte_cur bs=1 count=1 skip=$1 &>/dev/null
         cmp $byte_cur $byte_orig &>/dev/null
         if [[ $? != 0 ]]; then
             break
