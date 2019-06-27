abd0c74ffcc91868f313cce5bb5823f819d50322
Darren Ldl <darrenldldev@gmail.com>
2019-06-27 21:25:06 +1000

    Updated update_hash_tests

--- tests/update_hash_tests.sh
+++ tests/update_hash_tests.sh
@@ -4,6 +4,21 @@ exit_code=0
 
 VERSIONS=(1 2 3 17 18 19)
 
+HASHES=("sha1" "sha256" "sha512")
+if [[ $(command -v b2sum) != "" ]]; then
+    HASHES[3]="blake2b-256"
+    HASHES[4]="blake2b-512"
+fi
+
+# Record the hashes
+a[0]="SHA1 - "$(sha1sum   dummy | awk '{print $1}')
+a[1]="SHA256 - "$(sha256sum dummy | awk '{print $1}')
+a[2]="SHA512 - "$(sha512sum dummy | awk '{print $1}')
+if [[ $(command -v b2sum) != "" ]]; then
+    a[3]="BLAKE2b-256 - "$(b2sum -l 256 dummy | awk '{print $1}')
+    a[4]="BLAKE2b-512 - "$(b2sum        dummy | awk '{print $1}')
+fi
+
 # Encode in all 6 versions
 for ver in ${VERSIONS[*]}; do
   echo -n "Encoding in version $ver"
@@ -35,7 +50,7 @@ for ver in ${VERSIONS[*]}; do
         echo -n " ==> NOT okay"
         exit_code=1
     fi
-    if [[ $(echo $output | jq -r ".blocks[0].fileName") == "dummy" ]]; then
+    if [[ $(echo $output | jq -r ".blocks[0].hash") == ${a[1]} ]]; then
         echo " ==> Okay"
     else
         echo " ==> NOT okay"
@@ -47,49 +62,114 @@ new_fnm={}
 
 # Change hash type
 for ver in ${VERSIONS[*]}; do
-    echo -n "Changing file name of "dummy$ver.sbx
-    new_fnm[$ver]=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
-    output=$(./../blkar update --json -y --fnm ${new_fnm[$ver]} -v dummy$ver.sbx)
-    if [[ $(echo $output | jq -r ".error") != null ]]; then
-        echo " ==> Invalid JSON"
-        exit_code=1
-    fi
-    if [[ $(echo $output | jq -r ".stats.sbxVersion") == "$ver" ]]; then
-        echo -n " ==> Okay"
-    else
-        echo -n " ==> NOT okay"
-        exit_code=1
-    fi
-    if [[ $(echo $output | jq -r ".metadataChanges[0].changes[0].field") == "HSH" ]]; then
-        echo -n " ==> Okay"
-    else
-        echo -n " ==> NOT okay"
-        exit_code=1
-    fi
-    if [[ $(echo $output | jq -r ".metadataChanges[0].changes[0].from") == "dummy" ]]; then
-        echo -n " ==> Okay"
-    else
-        echo -n " ==> NOT okay"
-        exit_code=1
-    fi
-    if [[ $(echo $output | jq -r ".metadataChanges[0].changes[0].to") == ${new_fnm[$ver]} ]]; then
-        echo -n " ==> Okay"
-    else
-        echo -n " ==> NOT okay"
-        exit_code=1
-    fi
-    if [[ $(echo $output | jq -r ".metadataChanges[3]") == "null" ]]; then
-        echo -n " ==> Okay"
-    else
-        echo -n " ==> NOT okay"
-        exit_code=1
-    fi
-    if [[ $(echo $output | jq -r ".metadataChanges[0].changes[1]") == "null" ]]; then
-        echo " ==> Okay"
-    else
-        echo " ==> NOT okay"
-        exit_code=1
-    fi
+    i=0
+    for hash in ${HASHES[*]}; do
+        echo -n "Changing hash type of" dummy$ver.sbx "from sha256 to" $hash
+        output=$(./../blkar update --json -y --hash $hash -v dummy$ver.sbx)
+        if [[ $(echo $output | jq -r ".error") != null ]]; then
+            echo " ==> Invalid JSON"
+            exit_code=1
+        fi
+        if [[ $(echo $output | jq -r ".stats.sbxVersion") == "$ver" ]]; then
+            echo -n " ==> Okay"
+        else
+            echo -n " ==> NOT okay"
+            exit_code=1
+        fi
+        if [[ $hash == "sha256" ]]; then
+            if [[ $(echo $output | jq -r ".metadataChanges[0].changes") == "[]" ]]; then
+                echo -n " ==> Okay"
+            else
+                echo -n " ==> NOT okay"
+                exit_code=1
+            fi
+        else
+            if [[ $(echo $output | jq -r ".metadataChanges[0].changes[0].field") == "HSH" ]]; then
+                echo -n " ==> Okay"
+            else
+                echo -n " ==> NOT okay"
+                exit_code=1
+            fi
+            if [[ $(echo $output | jq -r ".metadataChanges[0].changes[0].from") == ${a[1]} ]]; then
+                echo -n " ==> Okay"
+            else
+                echo -n " ==> NOT okay"
+                exit_code=1
+            fi
+            if [[ $(echo $output | jq -r ".metadataChanges[0].changes[0].to") == ${a[$i]} ]]; then
+                echo -n " ==> Okay"
+            else
+                echo -n " ==> NOT okay"
+                exit_code=1
+            fi
+        fi
+        if [[ $(echo $output | jq -r ".metadataChanges[3]") == "null" ]]; then
+            echo -n " ==> Okay"
+        else
+            echo -n " ==> NOT okay"
+            exit_code=1
+        fi
+        if [[ $(echo $output | jq -r ".metadataChanges[0].changes[1]") == "null" ]]; then
+            echo " ==> Okay"
+        else
+            echo " ==> NOT okay"
+            exit_code=1
+        fi
+
+        # reset to default hash
+        echo -n "Changing hash type of" dummy$ver.sbx "from" $hash "to" sha256
+        output=$(./../blkar update --json -y --hash sha256 -v dummy$ver.sbx)
+        if [[ $(echo $output | jq -r ".error") != null ]]; then
+            echo " ==> Invalid JSON"
+            exit_code=1
+        fi
+        if [[ $(echo $output | jq -r ".stats.sbxVersion") == "$ver" ]]; then
+            echo -n " ==> Okay"
+        else
+            echo -n " ==> NOT okay"
+            exit_code=1
+        fi
+        if [[ $hash == "sha256" ]]; then
+            if [[ $(echo $output | jq -r ".metadataChanges[0].changes") == "[]" ]]; then
+                echo -n " ==> Okay"
+            else
+                echo -n " ==> NOT okay"
+                exit_code=1
+            fi
+        else
+            if [[ $(echo $output | jq -r ".metadataChanges[0].changes[0].field") == "HSH" ]]; then
+                echo -n " ==> Okay"
+            else
+                echo -n " ==> NOT okay"
+                exit_code=1
+            fi
+            if [[ $(echo $output | jq -r ".metadataChanges[0].changes[0].from") == ${a[$i]} ]]; then
+                echo -n " ==> Okay"
+            else
+                echo -n " ==> NOT okay"
+                exit_code=1
+            fi
+            if [[ $(echo $output | jq -r ".metadataChanges[0].changes[0].to") == ${a[1]} ]]; then
+                echo -n " ==> Okay"
+            else
+                echo -n " ==> NOT okay"
+                exit_code=1
+            fi
+        fi
+        if [[ $(echo $output | jq -r ".metadataChanges[3]") == "null" ]]; then
+            echo -n " ==> Okay"
+        else
+            echo -n " ==> NOT okay"
+            exit_code=1
+        fi
+        if [[ $(echo $output | jq -r ".metadataChanges[0].changes[1]") == "null" ]]; then
+            echo " ==> Okay"
+        else
+            echo " ==> NOT okay"
+            exit_code=1
+        fi
+        i=$[i+1]
+    done
 done
 
 # Show all
