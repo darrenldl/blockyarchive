2dbecab93cba9e0e8f2b22cbc467b28901500c03
Darren Ldl <darrenldldev@gmail.com>
2019-06-30 02:00:51 +1000

    Fixed gen_dummy.sh

--- tests/gen_dummy.sh
+++ tests/gen_dummy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 dd if=/dev/urandom of=dummy bs=$[1024 * 1024] count=2 &>/dev/null
-file_size=$[(1024 + RANDOM % 1000) + RANDOM % 1000]
+file_size=$[(1024 + RANDOM % 1000) * 1024 + RANDOM % 1000]
 echo $file_size > dummy_file_size
 truncate -s $file_size dummy
