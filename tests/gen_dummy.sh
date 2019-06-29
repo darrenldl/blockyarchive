95fd45b0ada16f1beb07f927245bb7579e9cc9d3
Darren Ldl <darrenldldev@gmail.com>
2019-06-30 00:12:17 +1000

    Added size randomisation for gen_dummy

--- tests/gen_dummy.sh
+++ tests/gen_dummy.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-dd if=/dev/urandom of=dummy bs=$[1024 * 1024 * 1] count=1 &>/dev/null
+dd if=/dev/urandom of=dummy bs=$[1024 * 1024] count=2 &>/dev/null
+file_size=$[(1024 + RANDOM % 1000) + RANDOM % 1000]
+echo "Using dummy file of size :" $file_size
+truncate -s $file_size dummy
