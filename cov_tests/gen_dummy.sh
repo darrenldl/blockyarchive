4f86c09e6a1f11375ba109c3e26063bc1e3b699b
Darren Ldl <darrenldldev@gmail.com>
2019-07-03 18:18:48 +1000

    Reduced dummy file size in cov_tests

--- cov_tests/gen_dummy.sh
+++ cov_tests/gen_dummy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dd if=/dev/urandom of=dummy bs=$[1024 * 1024] count=2 &>/dev/null
-file_size=$[(1024 + RANDOM % 1000) * 1024 + RANDOM % 1000]
+dd if=/dev/urandom of=dummy bs=$[1024 * 2] count=2 &>/dev/null
+file_size=$[(2 + RANDOM % 5) * 1024 + RANDOM % 1000]
 echo $file_size > dummy_file_size
 truncate -s $file_size dummy
