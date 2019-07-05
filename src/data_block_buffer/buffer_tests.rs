20dd6258bed1b966412ea877d610e6f3162b25a8
Darren Ldl <darrenldldev@gmail.com>
2019-07-05 17:34:56 +1000

    Added pt_stats_are_reset_correctly_after_buffer_reset

--- src/data_block_buffer/buffer_tests.rs
+++ src/data_block_buffer/buffer_tests.rs
@@ -76,6 +76,49 @@ proptest! {
     #[test]
     fn pt_stats_are_reset_correctly_after_buffer_reset(buffer_index in 1usize..1000,
                                                        total_buffer_count in 1usize..1000,
+                                                       data in 1usize..30,
+                                                       parity in 1usize..30,
+                                                       burst in 1usize..100,
                                                        fills in 1usize..1000) {
+        for buffer_case in 0..2 {
+            let mut buffer =
+                if buffer_case == 0 {
+                    DataBlockBuffer::new(Version::V1,
+                                         None,
+                                         InputType::Block,
+                                         OutputType::Block,
+                                         BlockArrangement::Unordered,
+                                         None,
+                                         true,
+                                         false,
+                                         buffer_index,
+                                         total_buffer_count,
+                    )
+                } else {
+                    DataBlockBuffer::new(Version::V17,
+                                         None,
+                                         InputType::Block,
+                                         OutputType::Block,
+                                         BlockArrangement::Unordered,
+                                         Some((data, parity, burst)),
+                                         true,
+                                         false,
+                                         buffer_index,
+                                         total_buffer_count,
+                    )
+                };
+
+            let size = buffer.lots.len() * buffer.lot_size;
+
+            let fill = std::cmp::min(size, fill);
+
+            for _ in 0..size {
+                let _ = buffer.get_slot();
+            }
+
+            lot.reset();
+
+            assert_eq!(buffer.lots_used, 0);
+        }
     }
 }
